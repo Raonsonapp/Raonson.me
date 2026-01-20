a7c0bcae4b14c8305dc8546bd8a5014e947b383f
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-21 03:03:50 +0500

    Create directive.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

---
+++ lib/engine/director/core/directive.dart
@@ -0,0 +1,11 @@
+class Directive {
+  final String action;
+  final String mood;
+  final int duration; // seconds
+
+  Directive({
+    required this.action,
+    required this.mood,
+    required this.duration,
+  });
+}
