32e5b3064c181fcadd7c907fdc7d02827ec2c1d8
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-21 02:51:32 +0500

    Create app_colors.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF6A00FF);
+  static const Color secondary = Color(0xFF00D4FF);
+  static const Color background = Colors.black;
+  static const Color textPrimary = Colors.white;
+}
