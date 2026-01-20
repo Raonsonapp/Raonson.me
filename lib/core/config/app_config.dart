71864a6b18cb9e78f15938c68411ef537e2d25c4
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-21 02:45:56 +0500

    Create app_config.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

---
+++ lib/core/config/app_config.dart
@@ -0,0 +1,9 @@
+class AppConfig {
+  static const String appName = 'Raonson';
+  static const String packageName = 'com.raonson.app';
+
+  static const bool isDebug = true;
+  static const bool enableLogs = true;
+
+  static const int apiTimeoutSeconds = 30;
+}
