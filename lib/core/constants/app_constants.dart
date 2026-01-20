1b0b51c11281a12ce852058d6dac6aee039f44c5
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-21 02:47:39 +0500

    Add files via upload
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  static const int maxUploadSizeMB = 2048;
+  static const int videoChunkSizeMB = 50;
+
+  static const Duration debounceDuration = Duration(milliseconds: 400);
+}
