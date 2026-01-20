deb722aef9fd4bd7a438cafcf7e4589a667e9d5a
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-21 03:09:44 +0500

    Create character_pose.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

---
+++ lib/engine/scene/core/character_pose.dart
@@ -0,0 +1,11 @@
+class CharacterPose {
+  final String characterId;
+  final String pose;
+  final String emotion;
+
+  CharacterPose({
+    required this.characterId,
+    required this.pose,
+    required this.emotion,
+  });
+}
