04f0f3614261fe0c81e523a647d41efe7649b28b
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-21 03:01:48 +0500

    Create animation_engine.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

---
+++ lib/engine/animation/services/animation_engine.dart
@@ -0,0 +1,8 @@
+import '../core/animation_clip.dart';
+
+class AnimationEngine {
+  Future<void> execute(AnimationClip clip) async {
+    // Placeholder: GPU / AI animation engine later
+    await Future.delayed(const Duration(milliseconds: 16));
+  }
+}
