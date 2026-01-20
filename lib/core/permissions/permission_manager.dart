c911379cbed2a0580fa9f8b98664a075c8027a0c
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-21 02:50:28 +0500

    Create permission_manager.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

---
+++ lib/core/permissions/permission_manager.dart
@@ -0,0 +1,8 @@
+import 'permission_types.dart';
+
+class PermissionManager {
+  static Future<bool> request(AppPermission permission) async {
+    // Real permission logic added later (platform specific)
+    return true;
+  }
+}
