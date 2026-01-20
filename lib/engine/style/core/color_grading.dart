13d6f7d7e6c1e408a13076432c261bf9091721e6
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-21 03:13:08 +0500

    Add files via upload
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

---
+++ lib/engine/style/core/color_grading.dart
@@ -0,0 +1,11 @@
+class ColorGrading {
+  final double contrast;
+  final double saturation;
+  final double brightness;
+
+  ColorGrading({
+    required this.contrast,
+    required this.saturation,
+    required this.brightness,
+  });
+}
---
+++ lib/engine/style/core/fx_preset.dart
@@ -0,0 +1,13 @@
+class FXPreset {
+  final String name;
+  final double glow;
+  final double motionBlur;
+  final double particleIntensity;
+
+  FXPreset({
+    required this.name,
+    required this.glow,
+    required this.motionBlur,
+    required this.particleIntensity,
+  });
+}
