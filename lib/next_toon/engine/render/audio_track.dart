e29ee5e29a26224a56099bb1e59d75a930cacb32
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-22 02:46:03 +0500

    Create audio_track.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

---
+++ lib/next_toon/engine/render/audio_track.dart
@@ -0,0 +1,11 @@
+class AudioTrack {
+  final String path;
+  final Duration start;
+  final Duration end;
+
+  AudioTrack({
+    required this.path,
+    required this.start,
+    required this.end,
+  });
+}
