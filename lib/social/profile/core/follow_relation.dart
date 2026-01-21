a382f6972137757008c26d59a7f7feb3612ef7ac
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-22 03:51:14 +0500

    Create follow_relation.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

---
+++ lib/social/profile/core/follow_relation.dart
@@ -0,0 +1,11 @@
+class FollowRelation {
+  final String followerId;
+  final String followingId;
+  final DateTime createdAt;
+
+  FollowRelation({
+    required this.followerId,
+    required this.followingId,
+    required this.createdAt,
+  });
+}
