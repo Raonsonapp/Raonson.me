e38dbbff2ba248fa850e7b96b5748499afb35c6a
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-22 03:40:43 +0500

    Create search_constants.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

---
+++ lib/search/core/search_constants.dart
@@ -0,0 +1,12 @@
+class SearchConstants {
+  static const int debounceMs = 400;
+
+  static const List<String> defaultCategories = [
+    'Anime',
+    'Games',
+    'Music',
+    'Movies',
+    'Programs',
+    'Live',
+  ];
+}
