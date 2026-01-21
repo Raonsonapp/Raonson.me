406eb942c34af1e53102946045f996f0d2642d81
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-22 02:21:47 +0500

    Create live_massage.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

---
+++ lib/live/models/live_massage.dart
@@ -0,0 +1,11 @@
+class LiveMessage {
+  final String userId;
+  final String text;
+  final DateTime time;
+
+  LiveMessage({
+    required this.userId,
+    required this.text,
+    required this.time,
+  });
+}
