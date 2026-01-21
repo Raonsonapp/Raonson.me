92cd6e866cb3f612a0096a957498a89af32796fd
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-22 03:21:03 +0500

    Delete lib/integration directory
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/integration/guards/nexttoon_guard.dart
+++
@@ -1,8 +0,0 @@
-import '../../engine/access/access_engine.dart';
-import '../../engine/access/core/user_role.dart';
-
-class NextToonGuard {
-  bool allow(UserRole role, bool premium) {
-    return AccessEngine.instance.canUseNextToon(role, premium);
-  }
-}
