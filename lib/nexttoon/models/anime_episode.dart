a15f9299655ecd11b73030b00cab0504f96017a5
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-22 03:04:08 +0500

    Add files via upload
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

---
+++ lib/nexttoon/models/anime_episode.dart
@@ -0,0 +1,11 @@
+import 'anime_scene.dart';
+
+class AnimeEpisode {
+  final String title;
+  final List<AnimeScene> scenes;
+
+  AnimeEpisode({
+    required this.title,
+    required this.scenes,
+  });
+}
---
+++ lib/nexttoon/models/anime_scene.dart
@@ -0,0 +1,11 @@
+class AnimeScene {
+  final int index;
+  final String description;
+  final int durationSeconds;
+
+  AnimeScene({
+    required this.index,
+    required this.description,
+    required this.durationSeconds,
+  });
+}
