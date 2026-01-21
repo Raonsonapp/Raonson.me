b8eb5a8500e7352b8cf23695e25459a19d38827a
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-22 03:26:56 +0500

    Create scene_access.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

---
+++ lib/nexttoon_advanced/scene_composer/scene_access.dart
@@ -0,0 +1,13 @@
+class SceneAsset {
+  final String id;
+  final String path;
+  final Duration start;
+  final Duration end;
+
+  SceneAsset({
+    required this.id,
+    required this.path,
+    required this.start,
+    required this.end,
+  });
+}
