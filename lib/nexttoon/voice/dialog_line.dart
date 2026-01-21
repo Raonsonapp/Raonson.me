4fe14dec3d727bc561a91ebb64d6bccd04a80aa6
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-22 03:10:59 +0500

    Create dialog_line.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

---
+++ lib/nexttoon/voice/dialog_line.dart
@@ -0,0 +1,11 @@
+class DialogLine {
+  final String text;
+  final Duration start;
+  final Duration duration;
+
+  DialogLine({
+    required this.text,
+    required this.start,
+    required this.duration,
+  });
+}
