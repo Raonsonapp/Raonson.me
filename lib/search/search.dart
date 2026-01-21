2a3718e7cec86aaee04b11ab9ac2f71e797a3e19
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-22 03:45:36 +0500

    Add files via upload
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

---
+++ lib/search/search.dart
@@ -0,0 +1,9 @@
+export 'models/search_category.dart';
+export 'models/search_item.dart';
+
+export 'services/search_service.dart';
+
+export 'state/search_state.dart';
+export 'state/search_controller.dart';
+
+export 'ui/search_page.dart';
---
+++ lib/search/search_exports.dart
@@ -0,0 +1,4 @@
+export 'ui/search_screen.dart';
+export 'services/search_engine.dart';
+export 'services/recommendation_engine.dart';
+export 'services/trending_engine.dart';
