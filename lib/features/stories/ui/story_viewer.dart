464e326f9235eb84f9e77f38f9c2908b513a1dab
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-29 22:01:41 +0500

    Update story_viewer.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/features/stories/ui/story_viewer.dart
+++ lib/features/stories/ui/story_viewer.dart
@@ -9,74 +9,81 @@ class StoryViewer extends StatefulWidget {
   State<StoryViewer> createState() => _StoryViewerState();
 }
 
-class _StoryViewerState extends State<StoryViewer>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
+class _StoryViewerState extends State<StoryViewer> {
   int index = 0;
 
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 5),
-    )..addStatusListener((status) {
-        if (status == AnimationStatus.completed) {
-          if (index < widget.story.mediaUrls.length - 1) {
-            setState(() => index++);
-            _controller.forward(from: 0);
-          } else {
-            Navigator.pop(context);
-          }
-        }
-      });
-    _controller.forward();
+  void next() {
+    if (index < widget.story.mediaUrls.length - 1) {
+      setState(() => index++);
+    } else {
+      Navigator.pop(context);
+    }
   }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+  void prev() {
+    if (index > 0) {
+      setState(() => index--);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: Image.network(
-              widget.story.mediaUrls[index],
-              fit: BoxFit.cover,
+      body: GestureDetector(
+        onTapUp: (d) {
+          final w = MediaQuery.of(context).size.width;
+          d.localPosition.dx < w / 2 ? prev() : next();
+        },
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Image.network(
+                widget.story.mediaUrls[index],
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          Positioned(
-            top: 40,
-            left: 10,
-            right: 10,
-            child: Row(
-              children: widget.story.mediaUrls.map((e) {
-                int i = widget.story.mediaUrls.indexOf(e);
-                return Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 2),
-                    child: LinearProgressIndicator(
-                      value: i < index
-                          ? 1
-                          : i == index
-                              ? _controller.value
-                              : 0,
-                      backgroundColor: Colors.white24,
-                      valueColor:
-                          const AlwaysStoppedAnimation<Color>(Colors.white),
+
+            // Progress bar
+            Positioned(
+              top: 40,
+              left: 8,
+              right: 8,
+              child: Row(
+                children: List.generate(
+                  widget.story.mediaUrls.length,
+                  (i) => Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 2),
+                      height: 3,
+                      color: i <= index ? Colors.white : Colors.white24,
                     ),
                   ),
-                );
-              }).toList(),
+                ),
+              ),
             ),
-          ),
-        ],
+
+            // Username
+            Positioned(
+              top: 60,
+              left: 16,
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 16,
+                    backgroundImage:
+                        NetworkImage(widget.story.avatarUrl),
+                  ),
+                  const SizedBox(width: 8),
+                  Text(
+                    widget.story.userName,
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
