3e28ce276fbd3ebb0b15e60fdbb0fb0bdebd5455
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-29 22:08:26 +0500

    Update media_picker.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/features/upload/ui/media_picker.dart
+++ lib/features/upload/ui/media_picker.dart
@@ -1,43 +1,42 @@
 import 'package:flutter/material.dart';
+import 'caption_screen.dart';
 
-class MediaPicker extends StatelessWidget {
-  const MediaPicker({super.key});
+class MediaPickerScreen extends StatelessWidget {
+  const MediaPickerScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // Preview area
-        AspectRatio(
-          aspectRatio: 1,
-          child: Container(
-            color: Colors.black12,
-            child: const Center(
-              child: Icon(Icons.photo, size: 80),
-            ),
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Select media'),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(2),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          crossAxisSpacing: 2,
+          mainAxisSpacing: 2,
         ),
-
-        const SizedBox(height: 8),
-
-        // Fake gallery grid (UI only)
-        Expanded(
-          child: GridView.builder(
-            itemCount: 30,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              crossAxisSpacing: 2,
-              mainAxisSpacing: 2,
-            ),
-            itemBuilder: (_, i) {
-              return Image.network(
-                'https://picsum.photos/300?random=$i',
-                fit: BoxFit.cover,
+        itemCount: 30,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => CaptionScreen(
+                    mediaId: index,
+                  ),
+                ),
               );
             },
-          ),
-        ),
-      ],
+            child: Container(
+              color: Colors.grey.shade300,
+              child: const Icon(Icons.image),
+            ),
+          );
+        },
+      ),
     );
   }
 }
