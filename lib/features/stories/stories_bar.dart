fc087053bbc90020342790f80038486647b9e2bb
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-29 15:14:45 +0500

    Update stories_bar.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/features/stories/stories_bar.dart
+++ lib/features/stories/stories_bar.dart
@@ -1,55 +1,38 @@
 import 'package:flutter/material.dart';
-import 'story_viewer.dart';
+import 'story_circle.dart';
+import 'story_model.dart';
 
 class StoriesBar extends StatelessWidget {
   const StoriesBar({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final stories = [
+      StoryModel(
+        username: 'your_story',
+        avatarUrl: 'https://i.pravatar.cc/150?img=1',
+      ),
+      StoryModel(
+        username: 'naruto',
+        avatarUrl: 'https://i.pravatar.cc/150?img=2',
+      ),
+      StoryModel(
+        username: 'luffy',
+        avatarUrl: 'https://i.pravatar.cc/150?img=3',
+      ),
+      StoryModel(
+        username: 'sasuke',
+        avatarUrl: 'https://i.pravatar.cc/150?img=4',
+      ),
+    ];
+
     return SizedBox(
-      height: 100,
+      height: 110,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: 10,
-        padding: const EdgeInsets.symmetric(horizontal: 8),
+        itemCount: stories.length,
         itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (_) => const StoryViewer(),
-                ),
-              );
-            },
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 6),
-              child: Column(
-                children: [
-                  Container(
-                    width: 68,
-                    height: 68,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      gradient: const LinearGradient(
-                        colors: [Colors.purple, Colors.red, Colors.orange],
-                      ),
-                    ),
-                    padding: const EdgeInsets.all(3),
-                    child: const CircleAvatar(
-                      backgroundColor: Colors.grey,
-                      child: Icon(Icons.person),
-                    ),
-                  ),
-                  const SizedBox(height: 6),
-                  const Text(
-                    'user',
-                    style: TextStyle(fontSize: 12),
-                  ),
-                ],
-              ),
-            ),
-          );
+          return StoryCircle(story: stories[index]);
         },
       ),
     );
