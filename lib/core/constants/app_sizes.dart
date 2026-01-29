826e91df33fe4c139e0b028e5b2abf241c20afac
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-29 20:51:59 +0500

    Create app_sizes.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

---
+++ lib/core/constants/app_sizes.dart
@@ -0,0 +1,12 @@
+class AppSizes {
+  static const double paddingXS = 4;
+  static const double paddingS = 8;
+  static const double paddingM = 16;
+  static const double paddingL = 24;
+
+  static const double avatarSmall = 32;
+  static const double avatarMedium = 48;
+  static const double avatarLarge = 72;
+
+  static const double iconSize = 24;
+}
