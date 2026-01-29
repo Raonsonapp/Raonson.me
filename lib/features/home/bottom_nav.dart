fb3279391eec78e907b46eda14b3aa026441efd6
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-29 14:58:21 +0500

    Create bottom_nav.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

---
+++ lib/features/home/bottom_nav.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class BottomNav extends StatelessWidget {
+  final int currentIndex;
+  final Function(int)? onTap;
+
+  const BottomNav({
+    super.key,
+    this.currentIndex = 0,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      currentIndex: currentIndex,
+      onTap: onTap,
+      selectedItemColor: Colors.black,
+      unselectedItemColor: Colors.grey,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_filled),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add_box_outlined),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.video_library_outlined),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline),
+          label: '',
+        ),
+      ],
+    );
+  }
+}
