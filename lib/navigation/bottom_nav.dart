69822ab9ce9378c5f4edbbb6959b6711d17fca42
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-29 22:32:50 +0500

    Update bottom_nav.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/navigation/bottom_nav.dart
+++ lib/navigation/bottom_nav.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
-// TEMP screens (ҳоло холӣ, баъд пурра мекунем)
+import '../features/feed/ui/feed_screen.dart';
+import '../features/search/ui/search_screen.dart';
+import '../features/reels/ui/reels_screen.dart';
 import '../features/upload/ui/upload_screen.dart';
+import '../features/profile/ui/profile_screen.dart';
 
 class BottomNav extends StatefulWidget {
   const BottomNav({super.key});
@@ -10,51 +13,54 @@ class BottomNav extends StatefulWidget {
   State<BottomNav> createState() => _BottomNavState();
 }
 
-class _BottomNavState extends State<BottomNav> {
+class _BottomNavState extends State<BottomNav>
+    with SingleTickerProviderStateMixin {
   int _index = 0;
 
-  final List<Widget> _screens = const [
-    _HomeScreen(),
-    _SearchScreen(),
+  late final AnimationController _controller =
+      AnimationController(vsync: this, duration: const Duration(milliseconds: 250));
+
+  final List<Widget> _pages = const [
+    FeedScreen(),
+    SearchScreen(),
+    ReelsScreen(),
     UploadScreen(),
-    _ReelsScreen(),
-    _ProfileScreen(),
+    ProfileScreen(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _screens[_index],
+      body: _pages[_index],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _index,
         type: BottomNavigationBarType.fixed,
         selectedItemColor: Colors.black,
         unselectedItemColor: Colors.grey,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        onTap: (i) => setState(() => _index = i),
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_filled),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_box_outlined, size: 30),
-            label: 'Upload',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.movie_outlined),
-            label: 'Reels',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_outline),
-            label: 'Profile',
-          ),
+        onTap: (i) {
+          setState(() => _index = i);
+          _controller.forward(from: 0);
+        },
+        items: [
+          _item(Icons.home_filled, 0),
+          _item(Icons.search, 1),
+          _item(Icons.video_library, 2),
+          _item(Icons.add_box_outlined, 3),
+          _item(Icons.person, 4),
         ],
+      ),
+    );
+  }
+
+  BottomNavigationBarItem _item(IconData icon, int i) {
+    final active = _index == i;
+    return BottomNavigationBarItem(
+      label: '',
+      icon: ScaleTransition(
+        scale: Tween<double>(begin: 1, end: 1.2).animate(
+          CurvedAnimation(parent: _controller, curve: Curves.easeOut),
+        ),
+        child: Icon(icon, size: active ? 28 : 24),
       ),
     );
   }
