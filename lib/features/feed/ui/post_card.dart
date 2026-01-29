1691b270fe48bad40c3887b07f937c9b5b2f2870
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-29 23:43:11 +0500

    Update post_card.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/features/feed/ui/post_card.dart
+++ lib/features/feed/ui/post_card.dart
@@ -1,125 +1,95 @@
 import 'package:flutter/material.dart';
-import '../data/post_model.dart';
 
 class PostCard extends StatefulWidget {
-  final PostModel post;
-  const PostCard({super.key, required this.post});
+  final String username;
+  final String imageUrl;
+  final String caption;
+  final int likes;
+
+  const PostCard({
+    super.key,
+    required this.username,
+    required this.imageUrl,
+    required this.caption,
+    required this.likes,
+  });
 
   @override
   State<PostCard> createState() => _PostCardState();
 }
 
-class _PostCardState extends State<PostCard>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _likeAnim;
-
-  @override
-  void initState() {
-    super.initState();
-    _likeAnim = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 300),
-      lowerBound: 0.8,
-      upperBound: 1.2,
-    );
-  }
-
-  void toggleLike() {
-    setState(() {
-      widget.post.isLiked = !widget.post.isLiked;
-    });
-    _likeAnim.forward(from: 0.8);
-  }
-
-  @override
-  void dispose() {
-    _likeAnim.dispose();
-    super.dispose();
-  }
+class _PostCardState extends State<PostCard> {
+  bool liked = false;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        // Header
         ListTile(
-          leading: CircleAvatar(
-            backgroundImage: NetworkImage(widget.post.userAvatar),
+          leading: const CircleAvatar(
+            backgroundImage: NetworkImage('https://picsum.photos/100'),
           ),
-          title: Text(widget.post.userName,
-              style: const TextStyle(fontWeight: FontWeight.bold)),
+          title: Text(
+            widget.username,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
           trailing: const Icon(Icons.more_vert),
         ),
-
-        // Image
         GestureDetector(
-          onDoubleTap: toggleLike,
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Image.network(widget.post.imageUrl,
-                  width: double.infinity, fit: BoxFit.cover),
-              ScaleTransition(
-                scale: _likeAnim,
-                child: Icon(
-                  Icons.favorite,
-                  size: 100,
-                  color: widget.post.isLiked
-                      ? Colors.white
-                      : Colors.transparent,
-                ),
-              ),
-            ],
+          onDoubleTap: () {
+            setState(() => liked = !liked);
+          },
+          child: Image.network(
+            widget.imageUrl,
+            width: double.infinity,
+            height: 350,
+            fit: BoxFit.cover,
           ),
         ),
-
-        // Actions
         Row(
           children: [
             IconButton(
               icon: Icon(
-                widget.post.isLiked
-                    ? Icons.favorite
-                    : Icons.favorite_border,
-                color: widget.post.isLiked ? Colors.red : null,
+                liked ? Icons.favorite : Icons.favorite_border,
+                color: liked ? Colors.red : null,
               ),
-              onPressed: toggleLike,
+              onPressed: () {
+                setState(() => liked = !liked);
+              },
             ),
             IconButton(
-              icon: const Icon(Icons.comment_outlined),
+              icon: const Icon(Icons.chat_bubble_outline),
               onPressed: () {},
             ),
             IconButton(
               icon: const Icon(Icons.send),
               onPressed: () {},
             ),
-            const Spacer(),
-            IconButton(
-              icon: const Icon(Icons.bookmark_border),
-              onPressed: () {},
-            ),
           ],
         ),
-
-        // Caption
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Text(
+            '${widget.likes + (liked ? 1 : 0)} likes',
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
           child: RichText(
             text: TextSpan(
-              style: const TextStyle(color: Colors.black),
+              style: DefaultTextStyle.of(context).style,
               children: [
                 TextSpan(
-                  text: widget.post.userName,
+                  text: widget.username,
                   style: const TextStyle(fontWeight: FontWeight.bold),
                 ),
-                const TextSpan(text: '  '),
-                TextSpan(text: widget.post.caption),
+                TextSpan(text: ' ${widget.caption}'),
               ],
             ),
           ),
         ),
-
         const SizedBox(height: 12),
       ],
     );
