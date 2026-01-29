33201847c245834735be4da9172f09d0adec4833
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-29 14:39:15 +0500

    Update home_screen.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
+import 'widgets/stories_bar.dart';
+import 'widgets/post_item.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(child: Text('Home', style: TextStyle(color: Colors.white))),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        elevation: 0,
+        title: const Text(
+          'Raonson',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        actions: const [
+          Icon(Icons.favorite_border),
+          SizedBox(width: 16),
+          Icon(Icons.send),
+          SizedBox(width: 12),
+        ],
+      ),
+      body: ListView(
+        children: const [
+          StoriesBar(),
+          Divider(color: Colors.grey),
+          PostItem(),
+          PostItem(),
+          PostItem(),
+        ],
+      ),
     );
   }
 }
