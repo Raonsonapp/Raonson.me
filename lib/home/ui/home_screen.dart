ea6cb14dd935a50809823755afe5b39f54bf4324
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-29 15:24:02 +0500

    Update home_screen.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/home/ui/home_screen.dart
+++ lib/home/ui/home_screen.dart
@@ -1,28 +1,41 @@
 import 'package:flutter/material.dart';
-import '../state/feed_controller.dart';
-import 'feed_list.dart';
+import 'story_bar.dart';
+import 'post_card.dart';
+import '../models/post_model.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  final controller = FeedController();
-
-  @override
-  void initState() {
-    super.initState();
-    controller.loadMore();
-  }
+  final List<PostModel> posts = List.generate(
+    5,
+    (index) => PostModel(
+      username: 'user$index',
+      userAvatar: 'https://i.pravatar.cc/150?img=$index',
+      imageUrl: 'https://picsum.photos/500/500?random=$index',
+      caption: 'This is a demo post number $index',
+      likes: 120 + index,
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Raonson')),
-      body: FeedList(controller: controller),
+      appBar: AppBar(
+        title: const Text('Raonson'),
+        actions: const [
+          Icon(Icons.favorite_border),
+          SizedBox(width: 16),
+          Icon(Icons.send),
+          SizedBox(width: 16),
+        ],
+      ),
+      body: ListView(
+        children: [
+          const StoryBar(),
+          const Divider(),
+          ...posts.map((post) => PostCard(post: post)),
+        ],
+      ),
     );
   }
 }
