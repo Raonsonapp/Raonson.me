606f4e8603bb4267efe44200c9a1b65930d1a8a9
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-29 15:06:16 +0500

    Update story_viewer.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/features/stories/story_viewer.dart
+++ lib/features/stories/story_viewer.dart
@@ -1,69 +1,61 @@
 import 'package:flutter/material.dart';
-import 'models/story_model.dart';
 
 class StoryViewer extends StatefulWidget {
-  final StoryModel story;
-  const StoryViewer({super.key, required this.story});
+  const StoryViewer({super.key});
 
   @override
   State<StoryViewer> createState() => _StoryViewerState();
 }
 
-class _StoryViewerState extends State<StoryViewer>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller =
-        AnimationController(vsync: this, duration: const Duration(seconds: 5))
-          ..forward()
-          ..addStatusListener((status) {
-            if (status == AnimationStatus.completed) {
-              Navigator.pop(context);
-            }
-          });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+class _StoryViewerState extends State<StoryViewer> {
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: GestureDetector(
-        onTap: () => Navigator.pop(context),
+        onTapUp: (_) {
+          setState(() {
+            index = (index + 1) % 5;
+          });
+        },
+        onVerticalDragEnd: (_) {
+          Navigator.pop(context);
+        },
         child: Stack(
           children: [
-            const Center(
-              child: Icon(Icons.image, size: 200, color: Colors.white24),
-            ),
-            Positioned(
-              top: 40,
-              left: 16,
-              right: 16,
-              child: LinearProgressIndicator(
-                value: _controller.value,
-                backgroundColor: Colors.white24,
-                valueColor:
-                    const AlwaysStoppedAnimation<Color>(Colors.white),
+            // STORY CONTENT
+            Center(
+              child: Text(
+                'Story ${index + 1}',
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 32,
+                ),
               ),
             ),
+
+            // TOP BAR
             Positioned(
-              top: 60,
-              left: 16,
+              top: 40,
+              left: 12,
+              right: 12,
               child: Row(
                 children: [
-                  const CircleAvatar(radius: 16),
+                  const CircleAvatar(
+                    radius: 18,
+                    backgroundColor: Colors.grey,
+                  ),
                   const SizedBox(width: 8),
-                  Text(
-                    widget.story.username,
-                    style: const TextStyle(color: Colors.white),
+                  const Text(
+                    'raonson',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  const Spacer(),
+                  IconButton(
+                    icon: const Icon(Icons.close, color: Colors.white),
+                    onPressed: () => Navigator.pop(context),
                   ),
                 ],
               ),
