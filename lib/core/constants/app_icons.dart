4bf02ed59436b6b98103a1df3d6cb08f27cee4be
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-29 20:51:37 +0500

    Create app_icons.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

---
+++ lib/core/constants/app_icons.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppIcons {
+  static const IconData home = Icons.home_filled;
+  static const IconData search = Icons.search;
+  static const IconData upload = Icons.add_box_outlined;
+  static const IconData reels = Icons.video_library_outlined;
+  static const IconData profile = Icons.person_outline;
+
+  static const IconData like = Icons.favorite_border;
+  static const IconData likeFilled = Icons.favorite;
+  static const IconData comment = Icons.chat_bubble_outline;
+  static const IconData share = Icons.send;
+  static const IconData bookmark = Icons.bookmark_border;
+}
