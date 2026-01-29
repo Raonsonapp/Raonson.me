13e1bc34261c4276608ea7b41c4ac7b328816b35
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-29 23:46:18 +0500

    Update user_model.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/features/profile/data/user_model.dart
+++ lib/features/profile/data/user_model.dart
@@ -1,21 +1,19 @@
 class UserModel {
-  final String id;
   final String username;
-  final String name;
   final String avatar;
   final String bio;
   final int posts;
   final int followers;
   final int following;
+  final bool verified;
 
   UserModel({
-    required this.id,
     required this.username,
-    required this.name,
     required this.avatar,
     required this.bio,
     required this.posts,
     required this.followers,
     required this.following,
+    this.verified = false,
   });
 }
