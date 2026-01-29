f141ff9afc9f781477ee94029ec51dee6b05022f
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-29 06:04:26 +0500

    Update main.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,63 @@
 import 'package:flutter/material.dart';
 
-// Фақат он экранеро import мекунем, ки воқеан ҳаст
 import 'screens/home_screen.dart';
+import 'screens/reels_screen.dart';
+import 'screens/profile_screen.dart';
 
 void main() {
-  runApp(RaonsonApp());
+  runApp(const RaonsonApp());
 }
 
-class RaonsonApp extends StatelessWidget {
+class RaonsonApp extends StatefulWidget {
+  const RaonsonApp({super.key});
+
+  @override
+  State<RaonsonApp> createState() => _RaonsonAppState();
+}
+
+class _RaonsonAppState extends State<RaonsonApp> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = const [
+    HomeScreen(),
+    ReelsScreen(),
+    ProfileScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Raonson',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Raonson'),
+          centerTitle: true,
+        ),
+        body: _screens[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.video_collection),
+              label: 'Reels',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+        ),
       ),
-      home: HomeScreen(),
     );
   }
 }
