ea939e813fcb03d2ed639ed99c0386d048a4994f
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-29 21:30:05 +0500

    Update feed_screen.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/features/feed/ui/feed_screen.dart
+++ lib/features/feed/ui/feed_screen.dart
@@ -1,35 +1,12 @@
 import 'package:flutter/material.dart';
-import '../../stories/ui/stories_bar.dart';
-import '../data/post_model.dart';
-import '../ui/post_card.dart';
-import '../../stories/data/story_model.dart';
+import 'stories_bar.dart';
+import 'post_card.dart';
 
 class FeedScreen extends StatelessWidget {
   const FeedScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final stories = List.generate(
-      10,
-      (i) => StoryModel(
-        id: '$i',
-        username: 'user$i',
-        avatarUrl: 'https://i.pravatar.cc/150?img=$i',
-      ),
-    );
-
-    final posts = List.generate(
-      5,
-      (i) => PostModel(
-        id: '$i',
-        username: 'user$i',
-        userAvatar: 'https://i.pravatar.cc/150?img=$i',
-        imageUrl: 'https://picsum.photos/500/500?random=$i',
-        caption: 'This is a sample caption #$i',
-        likes: 120 + i,
-      ),
-    );
-
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -38,16 +15,18 @@ class FeedScreen extends StatelessWidget {
         ),
         actions: const [
           Icon(Icons.favorite_border),
-          SizedBox(width: 12),
+          SizedBox(width: 16),
           Icon(Icons.send),
           SizedBox(width: 12),
         ],
       ),
       body: ListView(
-        children: [
-          StoriesBar(stories: stories),
-          const Divider(),
-          ...posts.map((p) => PostCard(post: p)),
+        children: const [
+          StoriesBar(),
+          Divider(height: 1),
+          PostCard(),
+          PostCard(),
+          PostCard(),
         ],
       ),
     );
