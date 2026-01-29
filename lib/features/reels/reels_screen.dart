547acd7f5e37355c080a00b75a2c0b08202612f5
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-29 14:52:38 +0500

    Update reels_screen.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/features/reels/reels_screen.dart
+++ lib/features/reels/reels_screen.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'reels_video_item.dart';
 
 class ReelsScreen extends StatelessWidget {
   const ReelsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(child: Text('Reels', style: TextStyle(color: Colors.white))),
+      body: PageView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return ReelsVideoItem(index: index);
+        },
+      ),
     );
   }
 }
