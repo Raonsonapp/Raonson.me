febbac7a264fd80612204ab3cc4a5402d56c0c3c
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-29 21:47:48 +0500

    Update post_model.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/features/feed/data/post_model.dart
+++ lib/features/feed/data/post_model.dart
@@ -1,19 +1,19 @@
 class PostModel {
-  final String id;
-  final String username;
+  final String userName;
   final String userAvatar;
   final String imageUrl;
   final String caption;
   final int likes;
+  final int comments;
   final bool isLiked;
 
   PostModel({
-    required this.id,
-    required this.username,
+    required this.userName,
     required this.userAvatar,
     required this.imageUrl,
     required this.caption,
     this.likes = 0,
+    this.comments = 0,
     this.isLiked = false,
   });
 }
