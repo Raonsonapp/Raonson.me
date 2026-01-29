2630ac0c7c853b0321cbdedeedbb893f6d04a8c6
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-29 20:58:56 +0500

    Create verified_badge.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

---
+++ lib/core/widgets/verified_badge.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class VerifiedBadge extends StatelessWidget {
+  final double size;
+
+  const VerifiedBadge({super.key, this.size = 14});
+
+  @override
+  Widget build(BuildContext context) {
+    return Icon(
+      Icons.verified,
+      size: size,
+      color: Colors.blue,
+    );
+  }
+}
