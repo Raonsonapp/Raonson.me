562e490137f1afaffb279342e98157a43d6cdf0d
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-31 02:58:29 +0500

    Update main.py
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- server/main.py
+++ server/main.py
@@ -1,3 +1,4 @@
+from api.reels import router as reels_router
 from api.stories import router as stories_router
 from api.upload import router as upload_router
 from api.feed import router as feed_router
@@ -63,3 +64,4 @@ app.include_router(upload_router)
 app.include_router(feed_router)
 app.include_router(stories_router)
 app.include_router(upload_router)
+app.include_router(reels_router)
