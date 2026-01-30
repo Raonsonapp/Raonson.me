fc9a6e00d35cc900d900c097bfcb76b4f731398a
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-31 01:13:55 +0500

    Create test_voices.py
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

---
+++ server/ai/nexttoon/test_voices.py
@@ -0,0 +1,7 @@
+from voice_profiles.hero_voice import HeroVoice
+from voice_profiles.villain_voice import VillainVoice
+from voice_profiles.narrator_voice import NarratorVoice
+
+print(HeroVoice.describe())
+print(VillainVoice.describe())
+print(NarratorVoice.describe())
