25531ef387d4f9284d4cf0fc52db9d2eba874d69
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-31 01:16:32 +0500

    Update test_voices.py
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- server/ai/nexttoon/test_voices.py
+++ server/ai/nexttoon/test_voices.py
@@ -2,6 +2,7 @@ from voice_profiles.hero_voice import HeroVoice
 from voice_profiles.villain_voice import VillainVoice
 from voice_profiles.narrator_voice import NarratorVoice
 
-print(HeroVoice.describe())
-print(VillainVoice.describe())
-print(NarratorVoice.describe())
+print("HERO:", HeroVoice.describe())
+print("VILLAIN:", VillainVoice.describe())
+print("NARRATOR:", NarratorVoice.describe())
+print("TEST OK")
