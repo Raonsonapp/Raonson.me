712187538e901bdb19906ab3cfeaf00419d51715
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-30 06:58:10 +0500

    Update app_strings.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -1,1 +1,7 @@
-class AppStrings{static const appName='Raonson';}
+class AppStrings {
+  static const appName = 'Raonson';
+  static const login = 'Login';
+  static const register = 'Register';
+  static const followers = 'Followers';
+  static const following = 'Following';
+}
