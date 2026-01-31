e8c120b8a6cad5eee0fd69221c5dae261d860efb
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-31 21:41:59 +0500

    Update bottom_nav.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/navigation/bottom_nav.dart
+++ lib/navigation/bottom_nav.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'app_router.dart';
+import '../features/feed/ui/feed_screen.dart';
+import '../features/search/ui/search_screen.dart';
+import '../features/upload/ui/upload_screen.dart';
+import '../features/reels/ui/reels_screen.dart';
+import '../features/profile/ui/profile_screen.dart';
 
 class BottomNav extends StatefulWidget {
   const BottomNav({super.key});
@@ -11,20 +15,33 @@ class BottomNav extends StatefulWidget {
 class _BottomNavState extends State<BottomNav> {
   int _index = 0;
 
+  final List<Widget> _screens = const [
+    FeedScreen(),
+    SearchScreen(),
+    UploadScreen(),
+    ReelsScreen(),
+    ProfileScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AppRouter.pages[_index],
+      body: _screens[_index],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _index,
         onTap: (i) => setState(() => _index = i),
         type: BottomNavigationBarType.fixed,
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-          BottomNavigationBarItem(icon: Icon(Icons.add_box), label: 'Upload'),
-          BottomNavigationBarItem(icon: Icon(Icons.video_library), label: 'Reels'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search), label: "Search"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_box_outlined), label: "Upload"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.play_circle_outline), label: "Reels"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person), label: "Profile"),
         ],
       ),
     );
