6149301673c80f4904c1302745d5fb7fa008a3c7
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-31 20:44:59 +0500

    Update feed_screen.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/features/feed/ui/feed_screen.dart
+++ lib/features/feed/ui/feed_screen.dart
@@ -1,1 +1,29 @@
-// Feed Screen placeholder
+import 'package:flutter/material.dart';
+import '../logic/feed_controller.dart';
+import 'post_card.dart';
+
+class FeedScreen extends StatefulWidget {
+  const FeedScreen({super.key});
+
+  @override
+  State<FeedScreen> createState() => _FeedScreenState();
+}
+
+class _FeedScreenState extends State<FeedScreen> {
+  final controller = FeedController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Raonson Feed")),
+      body: AnimatedBuilder(
+        animation: controller,
+        builder: (_, __) => ListView(
+          children: controller.posts
+              .map((p) => PostCard(post: p, controller: controller))
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
