da56eaa6ea64f46fc842f14dace52fbf4ad5ffcc
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-31 09:41:46 +0500

    Update follow.py
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- server/models/follow.py
+++ server/models/follow.py
@@ -1,9 +1,13 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, ForeignKey, UniqueConstraint
 from database import Base
 
 class Follow(Base):
     __tablename__ = "follows"
 
     id = Column(Integer, primary_key=True)
-    follower_id = Column(Integer)
-    following_id = Column(Integer)
+    follower_id = Column(Integer, ForeignKey("users.id"))
+    following_id = Column(Integer, ForeignKey("users.id"))
+
+    __table_args__ = (
+        UniqueConstraint("follower_id", "following_id"),
+    )
