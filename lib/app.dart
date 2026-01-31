007697515fc96fe172dd746651ec1b2765704a21
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-31 21:43:48 +0500

    Update app.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/app.dart
+++ lib/app.dart
@@ -1,1 +1,19 @@
-import 'package:flutter/material.dart';import 'core/theme/app_theme.dart';class RaonsonApp extends StatelessWidget{const RaonsonApp({super.key});@override Widget build(c)=>MaterialApp(title:'Raonson',theme:AppTheme.light,home:const Scaffold());}
+import 'package:flutter/material.dart';
+import 'navigation/bottom_nav.dart';
+
+class RaonsonApp extends StatelessWidget {
+  const RaonsonApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Raonson',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: const BottomNav(),
+    );
+  }
+}
