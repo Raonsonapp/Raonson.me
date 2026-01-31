d1e025fc15e680cd66d0d7f556f55caec7ac9cd8
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-31 10:27:27 +0500

    Create post.py
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

---
+++ server/models/post.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.sql import func
+from database import Base
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    caption = Column(String)
+    media_url = Column(String)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+  
