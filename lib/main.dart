d96c4013336a69921b96197d2c7fa89924a0ff2e
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-31 21:47:41 +0500

    Update main.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'features/home/ui/home_screen.dart';
+import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   runApp(const RaonsonApp());
-}
-
-class RaonsonApp extends StatelessWidget {
-  const RaonsonApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Raonson',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-      ),
-      home: const HomeScreen(),
-    );
-  }
 }
