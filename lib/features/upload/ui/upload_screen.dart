2b4b2481d6a6d527e83a45a2f371a0b3a3cede50
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-31 18:34:53 +0500

    Update upload_screen.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/features/upload/ui/upload_screen.dart
+++ lib/features/upload/ui/upload_screen.dart
@@ -1,1 +1,60 @@
-// Upload Screen placeholder
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class UploadScreen extends StatefulWidget {
+  const UploadScreen({super.key});
+
+  @override
+  State<UploadScreen> createState() => _UploadScreenState();
+}
+
+class _UploadScreenState extends State<UploadScreen> {
+  File? _image;
+  final picker = ImagePicker();
+
+  Future pickImage() async {
+    final picked = await picker.pickImage(source: ImageSource.gallery);
+
+    if (picked != null) {
+      setState(() {
+        _image = File(picked.path);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("New Post"),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text("Share"),
+          )
+        ],
+      ),
+
+      body: Column(
+        children: [
+
+          Expanded(
+            child: _image == null
+                ? const Center(child: Text("No Image Selected"))
+                : Image.file(_image!, fit: BoxFit.cover),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: ElevatedButton.icon(
+              onPressed: pickImage,
+              icon: const Icon(Icons.photo),
+              label: const Text("Choose from Gallery"),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
