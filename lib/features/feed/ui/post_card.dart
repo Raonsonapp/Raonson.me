e8272ebf925942f10cc5a790a007c09cf840e2cd
Raonson <ehsonmahmadmurodov@gmail.com>
2026-01-31 18:44:23 +0500

    Update post_card.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/features/feed/ui/post_card.dart
+++ lib/features/feed/ui/post_card.dart
@@ -9,83 +9,65 @@ class PostCard extends StatefulWidget {
 
 class _PostCardState extends State<PostCard> {
   bool liked = false;
-  int likeCount = 12;
+  int likes = 120;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // USER HEADER
+        ListTile(
+          leading: const CircleAvatar(child: Icon(Icons.person)),
+          title: const Text("Raonson User"),
+          trailing: const Icon(Icons.more_vert),
+        ),
 
-          // IMAGE
-          Container(
-            height: 200,
-            color: Colors.grey.shade300,
-            child: const Center(child: Text("POST IMAGE")),
-          ),
+        // IMAGE
+        Container(
+          height: 250,
+          color: Colors.grey.shade300,
+          child: const Center(child: Icon(Icons.image, size: 60)),
+        ),
 
-          const SizedBox(height: 8),
-
-          // ACTIONS ROW
-          Row(
-            children: [
-
-              IconButton(
-                icon: Icon(
-                  liked ? Icons.favorite : Icons.favorite_border,
-                  color: liked ? Colors.red : Colors.black,
-                ),
-                onPressed: () {
-                  setState(() {
-                    liked = !liked;
-                    liked ? likeCount++ : likeCount--;
-                  });
-                },
+        // ACTIONS
+        Row(
+          children: [
+            IconButton(
+              icon: Icon(
+                liked ? Icons.favorite : Icons.favorite_border,
+                color: liked ? Colors.red : null,
               ),
+              onPressed: () {
+                setState(() {
+                  liked = !liked;
+                  liked ? likes++ : likes--;
+                });
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.comment_outlined),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(Icons.share_outlined),
+              onPressed: () {},
+            ),
+          ],
+        ),
 
-              IconButton(
-                icon: const Icon(Icons.comment_outlined),
-                onPressed: () {
-                  showModalBottomSheet(
-                    context: context,
-                    builder: (_) => const _CommentSheet(),
-                  );
-                },
-              ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Text("$likes likes"),
+        ),
 
-            ],
-          ),
+        const Padding(
+          padding: EdgeInsets.all(12),
+          child: Text("This is Raonson post 🔥"),
+        ),
 
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12),
-            child: Text("$likeCount likes"),
-          ),
-
-          const SizedBox(height: 10),
-        ],
-      ),
-    );
-  }
-}
-
-class _CommentSheet extends StatelessWidget {
-  const _CommentSheet();
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 300,
-      child: Column(
-        children: const [
-          SizedBox(height: 10),
-          Text("Comments"),
-          Divider(),
-          ListTile(title: Text("User1: Nice!")),
-          ListTile(title: Text("User2: Cool 🔥")),
-        ],
-      ),
+        const Divider(),
+      ],
     );
   }
 }
