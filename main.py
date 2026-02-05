223461b8124d9feffa9b27f38fb621e6b5e27430
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-05 23:08:53 +0500

    Update main.py
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- main.py
+++ main.py
@@ -2,16 +2,14 @@ from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
-from database import SessionLocal, engine
+from database import SessionLocal, engine, Base
 from models import User
-from database import Base
+from security import hash_password, verify_password, create_access_token
 
 app = FastAPI()
 
-# CREATE TABLES
 Base.metadata.create_all(bind=engine)
 
-# DEPENDENCY
 def get_db():
     db = SessionLocal()
     try:
@@ -19,7 +17,6 @@ def get_db():
     finally:
         db.close()
 
-# ===== SCHEMAS =====
 class RegisterRequest(BaseModel):
     username: str
     password: str
@@ -28,7 +25,6 @@ class LoginRequest(BaseModel):
     username: str
     password: str
 
-# ===== ROOT =====
 @app.get("/")
 def root():
     return {"status": "Raonson server is running"}
@@ -37,36 +33,34 @@ def root():
 def health():
     return {"health": "ok"}
 
-# ===== AUTH =====
 @app.post("/auth/register")
 def register(data: RegisterRequest, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.username == data.username).first()
-    if user:
+    if db.query(User).filter(User.username == data.username).first():
         raise HTTPException(status_code=400, detail="User already exists")
 
-    new_user = User(
+    user = User(
         username=data.username,
-        password=data.password  # ҳоло plain, баъд hash мекунем
+        password=hash_password(data.password)
     )
-
-    db.add(new_user)
+    db.add(user)
     db.commit()
-    db.refresh(new_user)
+    db.refresh(user)
 
     return {
-        "message": "User registered successfully",
-        "id": new_user.id,
-        "username": new_user.username
+        "message": "Registered successfully",
+        "id": user.id
     }
 
 @app.post("/auth/login")
 def login(data: LoginRequest, db: Session = Depends(get_db)):
     user = db.query(User).filter(User.username == data.username).first()
 
-    if not user or user.password != data.password:
+    if not user or not verify_password(data.password, user.password):
         raise HTTPException(status_code=401, detail="Invalid credentials")
 
+    token = create_access_token({"user_id": user.id})
+
     return {
-        "message": "Login successful",
-        "user_id": user.id
-        }
+        "access_token": token,
+        "token_type": "bearer"
+    }
