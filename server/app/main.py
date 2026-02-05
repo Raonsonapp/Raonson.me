7e170c430bca883eb9c44c36942dda00a324d55d
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-05 18:14:07 +0500

    Update main.py
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- server/app/main.py
+++ server/app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
-from app.config import settings
 
-app = FastAPI(title=settings.PROJECT_NAME)
+app = FastAPI(title="Raonson Server")
 
 @app.get("/")
 def root():
     return {
         "status": "Raonson server is running 🚀"
     }
+
+@app.get("/health")
+def health():
+    return {"ok": True}
