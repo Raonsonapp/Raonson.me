0087bd7791c6a8f3b8f075df3b1a965b74104052
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-05 19:35:59 +0500

    Update main.py
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- server/main.py
+++ server/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from app.database import engine, Base
 
 app = FastAPI(title="Raonson API", version="1.0.0")
+
+# 👇 база ҳангоми старт сохта мешавад
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
