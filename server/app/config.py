abb59c0effee7d8e5b47979650a1e4eef5e4a99a
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-05 19:31:12 +0500

    Update config.py
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- server/app/config.py
+++ server/app/config.py
@@ -1,15 +1,4 @@
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
-class Settings:
-    PROJECT_NAME = "Raonson"
-    SECRET_KEY = os.getenv("SECRET_KEY", "raonson_secret")
-    ALGORITHM = "HS256"
-    DATABASE_URL = os.getenv(
-        "DATABASE_URL",
-        "postgresql://postgres:password@localhost:5432/raonson"
-    )
-
-settings = Settings()
+PROJECT_NAME = "Raonson"
+DEBUG = True
