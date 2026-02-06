d047624161eae0417884af92178ec7e81521515d
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-06 23:51:03 +0500

    Update chat_screen.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/screens/chat/chat_screen.dart
+++ lib/screens/chat/chat_screen.dart
@@ -1,54 +1,10 @@
 import 'package:flutter/material.dart';
-import '../../services/chat_service.dart';
-import 'chat_detail_screen.dart';
 
-class ChatScreen extends StatelessWidget {
-  const ChatScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Chats")),
-      body: FutureBuilder<List>(
-        future: ChatService.getConversations(),
-        builder: (context, snap) {
-          if (snap.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-
-          if (snap.hasError) {
-            return const Center(child: Text("Error loading chats"));
-          }
-
-          final chats = snap.data!;
-          if (chats.isEmpty) {
-            return const Center(child: Text("No chats yet"));
-          }
-
-          return ListView.builder(
-            itemCount: chats.length,
-            itemBuilder: (context, i) {
-              final c = chats[i];
-              return ListTile(
-                leading: const CircleAvatar(child: Icon(Icons.person)),
-                title: Text("Chat"),
-                subtitle: Text("Tap to open"),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => ChatDetailScreen(
-                        conversationId: c["id"],
-                        toUserId: c["user1_id"],
-                      ),
-                    ),
-                  );
-                },
-              );
-            },
-          );
-        },
-      ),
-    );
+    return const Center(child: Text('HOME'));
   }
 }
