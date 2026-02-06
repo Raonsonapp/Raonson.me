d8ec1833a29732545f6cabf85e5d3dc09230d619
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-06 22:39:40 +0500

    Update main.py
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from database import Base, engine
 from routers.auth import router as auth_router
 from routers.posts import router as posts_router
 from routers.profile import router as profile_router
+from routers.search import router as search_router
 
 app = FastAPI(title="Raonson v2 Backend")
 
@@ -15,3 +16,4 @@ def root():
     return {"status": "Raonson v2 backend running"}
 app.include_router(posts_router)
 app.include_router(profile_router)
+app.include_router(search_router)
