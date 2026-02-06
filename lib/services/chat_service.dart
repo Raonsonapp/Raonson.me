3d7b36160083ff08ef20f001b2cf0e52ee1b6f49
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-06 22:45:51 +0500

    Update chat_service.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/services/chat_service.dart
+++ lib/services/chat_service.dart
@@ -1,18 +1,14 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'api.dart';
+import '../config/api.dart';
+import 'auth_service.dart';
 
 class ChatService {
-  static Future<List<dynamic>> getChats(int userId) async {
-    final response = await http.get(
-      Uri.parse("${Api.baseUrl}/chats/$userId"),
-      headers: {"Accept": "application/json"},
+  static Future<List> getConversations() async {
+    final res = await http.get(
+      Uri.parse("$baseUrl/chat/conversations"),
+      headers: {"Authorization": AuthService.token!},
     );
-
-    if (response.statusCode == 200) {
-      return jsonDecode(response.body);
-    } else {
-      throw Exception("Failed to load chats");
-    }
+    return jsonDecode(res.body);
   }
 }
