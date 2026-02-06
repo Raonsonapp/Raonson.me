4798bdbb6259b73ba86d4ddd413a3d52f7c9a886
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-06 21:11:09 +0500

    Update models.py
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- server/models.py
+++ server/models.py
@@ -3,30 +3,27 @@ from database import Base
 
 class User(Base):
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True)
     username = Column(String, unique=True, index=True)
     password = Column(String)
+    bio = Column(Text, default="")
 
 class Post(Base):
     __tablename__ = "posts"
-
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
+    user_id = Column(Integer)
     image = Column(String)
     caption = Column(Text)
 
 class Reel(Base):
     __tablename__ = "reels"
-
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
+    user_id = Column(Integer)
     video = Column(String)
     caption = Column(Text)
 
 class Chat(Base):
     __tablename__ = "chats"
-
     id = Column(Integer, primary_key=True)
     from_user = Column(Integer)
     to_user = Column(Integer)
