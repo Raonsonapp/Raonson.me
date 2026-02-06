5790a2ec5e224c2b2bdae56bcfce3e3efa777486
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-06 17:07:55 +0500

    Update home_screen.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,9 +5,25 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: AppBar(title: Text('Raonson')),
-      body: Center(child: Text('Home Feed')),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Raonson'),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 12),
+            child: Icon(Icons.add_box_outlined),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: const [
+          SizedBox(height: 10),
+          StoriesBar(),
+          Divider(color: Colors.white12),
+          PostCard(),
+          PostCard(),
+        ],
+      ),
     );
   }
 }
