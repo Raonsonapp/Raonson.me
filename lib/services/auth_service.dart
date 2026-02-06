90ded0632abf032ee19dc154add7cc3a04266b72
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-06 22:07:42 +0500

    Update auth_service.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,17 +1,34 @@
-import 'api_service.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class AuthService {
-  static Future<dynamic> login(String username, String password) {
-    return ApiService.post('/auth/login', {
-      'username': username,
-      'password': password,
-    });
+  static const String baseUrl = 'https://raonson-me.onrender.com';
+
+  // REGISTER
+  static Future<bool> register(String username, String password) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/auth/register'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'username': username,
+        'password': password,
+      }),
+    );
+
+    return response.statusCode == 200;
   }
 
-  static Future<dynamic> register(String username, String password) {
-    return ApiService.post('/auth/register', {
-      'username': username,
-      'password': password,
-    });
+  // LOGIN
+  static Future<bool> login(String username, String password) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/auth/login'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'username': username,
+        'password': password,
+      }),
+    );
+
+    return response.statusCode == 200;
   }
 }
