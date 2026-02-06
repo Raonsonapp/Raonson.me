ca0226f2d58d3310bd16d48e06a5dc9137c0390a
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-06 21:06:07 +0500

    Update models.py
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- server/models.py
+++ server/models.py
@@ -1,9 +1,33 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Text, ForeignKey
 from database import Base
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     username = Column(String, unique=True, index=True)
     password = Column(String)
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    image = Column(String)
+    caption = Column(Text)
+
+class Reel(Base):
+    __tablename__ = "reels"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    video = Column(String)
+    caption = Column(Text)
+
+class Chat(Base):
+    __tablename__ = "chats"
+
+    id = Column(Integer, primary_key=True)
+    from_user = Column(Integer)
+    to_user = Column(Integer)
+    message = Column(Text)
