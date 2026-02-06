b8cff9214dd7290a39131d6ae96735d7989da7ba
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-06 23:51:30 +0500

    Update profile_screen.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,33 +1,10 @@
 import 'package:flutter/material.dart';
-import '../../services/profile_service.dart';
 
-class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Profile")),
-      body: FutureBuilder<Map<String, dynamic>>(
-        future: ProfileService.myProfile(),
-        builder: (context, snap) {
-          if (!snap.hasData) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          final u = snap.data!;
-          return Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(u["username"], style: const TextStyle(fontSize: 20)),
-                const SizedBox(height: 8),
-                Text("Posts: ${u["posts_count"]}"),
-              ],
-            ),
-          );
-        },
-      ),
-    );
+    return const Center(child: Text('HOME'));
   }
 }
