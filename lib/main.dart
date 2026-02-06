3d5300c923ef848406a257cfdfb294b96f257ba7
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-06 23:46:44 +0500

    Update main.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,24 @@
 import 'package:flutter/material.dart';
-import 'screens/home/home_screen.dart';
-import 'screens/search/search_screen.dart';
-import 'screens/chat/chat_screen.dart';
-import 'screens/profile/profile_screen.dart';
+import 'navigation/app_router.dart';
 
-void main() => runApp(const RaonsonApp());
-
-class RaonsonApp extends StatefulWidget {
-  const RaonsonApp({super.key});
-  @override
-  State<RaonsonApp> createState() => _RaonsonAppState();
+void main() {
+  runApp(const RaonsonApp());
 }
 
-class _RaonsonAppState extends State<RaonsonApp> {
-  int i = 0;
-  final pages = const [
-    HomeScreen(),
-    SearchScreen(),
-    ChatScreen(),
-    ProfileScreen(),
-  ];
+class RaonsonApp extends StatelessWidget {
+  const RaonsonApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: pages[i],
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: i,
-          onTap: (v) => setState(() => i = v),
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-            BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
-            BottomNavigationBarItem(icon: Icon(Icons.chat), label: "Chat"),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
-          ],
-        ),
+      title: 'Raonson',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: const Color(0xFF0B0F1A),
+        primaryColor: const Color(0xFF4DA3FF),
       ),
+      home: const AppRouter(),
     );
   }
 }
