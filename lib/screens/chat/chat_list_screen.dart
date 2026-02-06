1f71327ab53edcab64501c16c4b1f919b7c16c6e
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 00:04:16 +0500

    Update chat_list_screen.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/screens/chat/chat_list_screen.dart
+++ lib/screens/chat/chat_list_screen.dart
@@ -7,37 +7,24 @@ class ChatListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFF0B0F1A),
       appBar: AppBar(
         backgroundColor: const Color(0xFF0B0F1A),
         elevation: 0,
         title: const Text('Chats'),
-        actions: const [
-          Icon(Icons.edit),
-          SizedBox(width: 16),
-        ],
       ),
       body: ListView.builder(
         itemCount: 10,
-        itemBuilder: (context, index) {
+        itemBuilder: (context, i) {
           return ListTile(
-            leading: const CircleAvatar(
-              radius: 22,
-              child: Icon(Icons.person),
-            ),
-            title: Text(
-              'user$index',
-              style: const TextStyle(fontWeight: FontWeight.w500),
-            ),
-            subtitle: const Text(
-              'Last message preview...',
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-            ),
+            leading: const CircleAvatar(child: Icon(Icons.person)),
+            title: Text('user$i'),
+            subtitle: const Text('Last message...'),
             onTap: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (_) => ChatScreen(username: 'user$index'),
+                  builder: (_) => ChatScreen(username: 'user$i'),
                 ),
               );
             },
