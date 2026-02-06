8db85d06b527f5a83e0a078a845fcb002beef6a2
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-06 16:46:51 +0500

    Update posts.py
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- server/app/routes/posts.py
+++ server/app/routes/posts.py
@@ -1,31 +1,19 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 from app.schemas.post import PostCreate, PostOut
-from datetime import datetime
+from app.services.post_service import create_post, list_posts
 
 router = APIRouter(prefix="/posts", tags=["Posts"])
 
-# FAKE STORAGE
-POSTS = []
-POST_ID = 1
-
 @router.post("", response_model=PostOut)
-def create_post(data: PostCreate):
-    global POST_ID
-
-    post = {
-        "id": POST_ID,
-        "user_id": 1,  # ҳоло fake
-        "caption": data.caption,
-        "image_url": data.image_url,
-        "created_at": datetime.utcnow()
-    }
-
-    POSTS.append(post)
-    POST_ID += 1
-
+def create(data: PostCreate):
+    post = create_post(
+        user_id=1,  # ҳоло fake
+        caption=data.caption,
+        image_url=data.image_url
+    )
     return post
 
 
 @router.get("", response_model=list[PostOut])
-def list_posts():
-    return POSTS
+def list_all():
+    return list_posts()
