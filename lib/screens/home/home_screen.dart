1838cea79cc7a0de6c5bf12bc3e1a5e7f08d296b
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-06 23:50:30 +0500

    Update home_screen.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,38 +1,10 @@
 import 'package:flutter/material.dart';
-import '../../services/post_service.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Raonson")),
-      body: FutureBuilder<List>(
-        future: PostService.getFeed(),
-        builder: (context, snap) {
-          if (!snap.hasData) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          final posts = snap.data!;
-          return ListView.builder(
-            itemCount: posts.length,
-            itemBuilder: (_, i) {
-              final p = posts[i];
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Image.network(p["image_url"]),
-                  Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Text(p["caption"] ?? ""),
-                  ),
-                ],
-              );
-            },
-          );
-        },
-      ),
-    );
+    return const Center(child: Text('HOME'));
   }
 }
