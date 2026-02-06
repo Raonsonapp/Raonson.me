2a97594d0e257dc90ee1fdcdb66e044e6ca15cf4
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-06 19:00:53 +0500

    Update app_theme.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,29 +1,15 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static ThemeData darkBlueTheme = ThemeData(
-    brightness: Brightness.dark,
-    scaffoldBackgroundColor: const Color(0xFF0A0F1F),
-    primaryColor: const Color(0xFF1E90FF),
-    fontFamily: 'Poppins',
-    appBarTheme: const AppBarTheme(
-      backgroundColor: Colors.transparent,
-      elevation: 0,
-      centerTitle: true,
-      titleTextStyle: TextStyle(
-        color: Colors.white,
-        fontSize: 22,
-        fontWeight: FontWeight.w600,
-        letterSpacing: 1.2,
+  static ThemeData get darkTheme {
+    return ThemeData(
+      brightness: Brightness.dark,
+      scaffoldBackgroundColor: const Color(0xFF0A0F1F),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.black,
+        elevation: 0,
       ),
-    ),
-    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-      backgroundColor: Color(0xFF0A0F1F),
-      selectedItemColor: Color(0xFF4FC3F7),
-      unselectedItemColor: Colors.white54,
-      showSelectedLabels: false,
-      showUnselectedLabels: false,
-      elevation: 0,
-    ),
-  );
+      useMaterial3: true,
+    );
+  }
 }
