db81306c5bd62e893921dd6e44d85245a6256f2b
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-06 23:47:44 +0500

    Update app_router.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/navigation/app_router.dart
+++ lib/navigation/app_router.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
-import '../features/feed/ui/feed_screen.dart';
-import '../features/reels/ui/reels_screen.dart';
-import '../features/upload/ui/upload_screen.dart';
-import '../features/search/ui/search_screen.dart';
-import '../features/profile/ui/profile_screen.dart';
+import '../screens/auth/login_screen.dart';
+import 'bottom_nav.dart';
 
-class AppRouter {
-  static final pages = <Widget>[
-    const FeedScreen(),
-    const SearchScreen(),
-    const UploadScreen(),
-    const ReelsScreen(),
-    const ProfileScreen(),
-  ];
+class AppRouter extends StatefulWidget {
+  const AppRouter({super.key});
+
+  @override
+  State<AppRouter> createState() => _AppRouterState();
+}
+
+class _AppRouterState extends State<AppRouter> {
+  bool isLoggedIn = false; // v2: баъд аз server
+
+  @override
+  Widget build(BuildContext context) {
+    if (isLoggedIn) {
+      return const BottomNav();
+    } else {
+      return LoginScreen(
+        onLoginSuccess: () {
+          setState(() {
+            isLoggedIn = true;
+          });
+        },
+      );
+    }
+  }
 }
