43e0fac41436de01e62a8a157e458ed633c270b9
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 22:38:48 +0500

    Update post_service.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/services/post_service.dart
+++ lib/services/post_service.dart
@@ -1,52 +1,18 @@
-import '../core/api.dart';
 import '../core/http_service.dart';
+import '../core/api.dart';
 
 class PostService {
-  // ===== GET ALL POSTS =====
   static Future<List<dynamic>> getAll() async {
-    final res = await HttpService.get(Api.posts);
-    return (res as List?) ?? [];
+    return await HttpService.get('${Api.baseUrl}/posts');
   }
 
-  // ===== CREATE POST =====
-  static Future<void> create({
-    required String username,
-    required String imageUrl,
-    required String caption,
-  }) async {
-    await HttpService.post(
-      Api.posts,
-      {
-        'username': username,
-        'image_url': imageUrl,
-        'caption': caption,
-      },
-    );
+  static Future<List<dynamic>> getReels() async {
+    return await HttpService.get('${Api.baseUrl}/reels');
   }
 
-  // ===== LIKE POST =====
-  static Future<void> like(int postId) async {
-    await HttpService.post(Api.likePost(postId), {});
-  }
-
-  // ===== GET COMMENTS =====
-  static Future<List<dynamic>> comments(int postId) async {
-    final res = await HttpService.get(Api.comments(postId));
-    return (res as List?) ?? [];
-  }
-
-  // ===== ADD COMMENT =====
-  static Future<void> addComment({
-    required int postId,
-    required String username,
-    required String text,
-  }) async {
-    await HttpService.post(
-      Api.comments(postId),
-      {
-        'username': username,
-        'text': text,
-      },
+  static Future<List<dynamic>> getByUser(String username) async {
+    return await HttpService.get(
+      '${Api.baseUrl}/users/$username/posts',
     );
   }
 }
