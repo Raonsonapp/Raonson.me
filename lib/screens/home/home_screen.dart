83e44501cc568fbcdad32762f6291c08b652cada
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 11:26:46 +0500

    Update home_screen.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,23 +1,71 @@
 import 'package:flutter/material.dart';
+import '../../core/api.dart';
+import '../../core/http_client.dart';
+import '../../models/post.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Post> posts = [];
+  bool loading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    load();
+  }
+
+  Future<void> load() async {
+    final data = await HttpClient.get(
+      '${ApiConfig.baseUrl}/posts',
+    );
+
+    setState(() {
+      posts = data.map<Post>((e) => Post.fromJson(e)).toList();
+      loading = false;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFF0B0F1A),
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF0B0F1A),
-        elevation: 0,
-        title: const Text('Raonson'),
-      ),
-      body: const Center(
-        child: Text(
-          'Home feed (v2)',
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
+    if (loading) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    return ListView.builder(
+      itemCount: posts.length,
+      itemBuilder: (c, i) {
+        final p = posts[i];
+        return Card(
+          color: const Color(0xFF11162A),
+          margin: const EdgeInsets.all(12),
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  p.username,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  p.caption,
+                  style: const TextStyle(color: Colors.white70),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
