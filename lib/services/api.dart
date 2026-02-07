f47faea2486c8f85394e43b91ee94236f7a7c7f8
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 19:43:04 +0500

    Update api.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -3,4 +3,7 @@ class Api {
   static const reels = '$baseUrl/reels';
   static const search = '$baseUrl/search';
   static const me = '$baseUrl/me';
+  static const chats = '$baseUrl/chats';
+  static const messages = '$baseUrl/messages';
+  static const sendMessage = '$baseUrl/messages/send';
 }
