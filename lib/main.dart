80b90c19dfdbd825d245c27b2d178afabb597565
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 14:35:21 +0500

    Update main.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'core/session.dart';
+import 'screens/auth/login_screen.dart';
 import 'screens/home/home_screen.dart';
 
 void main() {
@@ -13,7 +15,40 @@ class RaonsonApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
-      home: const HomeScreen(),
+      home: const _Bootstrap(),
     );
+  }
+}
+
+class _Bootstrap extends StatefulWidget {
+  const _Bootstrap();
+
+  @override
+  State<_Bootstrap> createState() => _BootstrapState();
+}
+
+class _BootstrapState extends State<_Bootstrap> {
+  bool loading = true;
+  bool logged = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _check();
+  }
+
+  Future<void> _check() async {
+    logged = await Session.isLoggedIn();
+    setState(() => loading = false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (loading) {
+      return const Scaffold(
+        body: Center(child: CircularProgressIndicator()),
+      );
+    }
+    return logged ? const HomeScreen() : const LoginScreen();
   }
 }
