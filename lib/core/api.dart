84a3375bb063818ff70ccdba580cab97e620749e
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 17:47:25 +0500

    Update api.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/core/api.dart
+++ lib/core/api.dart
@@ -1,3 +1,17 @@
 class Api {
-  static const String baseUrl = 'https://raonson-me.onrender.com';
+  static const String baseUrl =
+      'https://raonson-me.onrender.com';
+
+  // Auth
+  static const String register = '/auth/register';
+  static const String login = '/auth/login';
+
+  // Posts
+  static const String posts = '/posts';
+
+  // Reels
+  static const String reels = '/reels';
+
+  // Chat
+  static const String chats = '/chats';
 }
