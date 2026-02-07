73cc9d9ae0fd2adc7a8cc437c206d6c38794c40d
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 17:42:45 +0500

    Update bottom_nav.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/navigation/bottom_nav.dart
+++ lib/navigation/bottom_nav.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 
-// Screens
 import '../screens/home/home_screen.dart';
 import '../screens/reels/reels_screen.dart';
 import '../screens/search/search_screen.dart';
-import '../screens/chat/chat_list_screen.dart';
 import '../screens/profile/profile_screen.dart';
 
 class BottomNav extends StatefulWidget {
@@ -15,92 +13,60 @@ class BottomNav extends StatefulWidget {
 }
 
 class _BottomNavState extends State<BottomNav> {
-  int _currentIndex = 0;
+  int _index = 0;
 
-  // ================= SCREENS =================
   final List<Widget> _screens = const [
-    HomeScreen(),        // 0
-    ChatListScreen(),    // 1
-    ReelsScreen(),       // 2
-    SearchScreen(),      // 3
-    ProfileScreen(),     // 4
+    HomeScreen(),
+    ReelsScreen(),
+    SearchScreen(),
+    ProfileScreen(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF0B0F1A),
-      body: IndexedStack(
-        index: _currentIndex,
-        children: _screens,
-      ),
-      bottomNavigationBar: _bottomBar(),
-    );
-  }
 
-  // ================= BOTTOM BAR =================
-  Widget _bottomBar() {
-    return Container(
-      decoration: const BoxDecoration(
-        color: Color(0xFF0B0F1A),
-        boxShadow: [
-          BoxShadow(
-            color: Color(0x663AA0FF), // glow кабуд ба боло
-            blurRadius: 18,
-            offset: Offset(0, -6),
+      body: _screens[_index],
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _index,
+        onTap: (i) {
+          setState(() => _index = i);
+        },
+
+        backgroundColor: const Color(0xFF0B0F1A),
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white38,
+
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            activeIcon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.play_circle_outline),
+            activeIcon: Icon(Icons.play_circle),
+            label: 'Reels',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: CircleAvatar(
+              radius: 12,
+              backgroundColor: Colors.blueAccent,
+              child: Icon(Icons.person, size: 14),
+            ),
+            label: 'Profile',
           ),
         ],
-      ),
-      child: SafeArea(
-        top: false,
-        child: BottomNavigationBar(
-          currentIndex: _currentIndex,
-          onTap: (index) {
-            setState(() => _currentIndex = index);
-          },
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: const Color(0xFF0B0F1A),
-          elevation: 0,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          selectedItemColor: const Color(0xFF3AA0FF),
-          unselectedItemColor: Colors.white38,
-          items: const [
-            // HOME
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home_outlined),
-              activeIcon: Icon(Icons.home),
-              label: '',
-            ),
-
-            // CHAT (мисли Instagram airplane)
-            BottomNavigationBarItem(
-              icon: Icon(Icons.send_outlined),
-              activeIcon: Icon(Icons.send),
-              label: '',
-            ),
-
-            // REELS
-            BottomNavigationBarItem(
-              icon: Icon(Icons.play_circle_outline),
-              activeIcon: Icon(Icons.play_circle),
-              label: '',
-            ),
-
-            // SEARCH
-            BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: '',
-            ),
-
-            // PROFILE
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person_outline),
-              activeIcon: Icon(Icons.person),
-              label: '',
-            ),
-          ],
-        ),
       ),
     );
   }
