3fdbe47fd71f31ba77272f345cb706319df1eccc
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 19:45:39 +0500

    Update bottom_nav.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/navigation/bottom_nav.dart
+++ lib/navigation/bottom_nav.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import '../screens/home/home_screen.dart'
+
+import '../screens/home/home_screen.dart';
 import '../screens/reels/reels_screen.dart';
+import '../screens/chat/chat_list_screen.dart';
 import '../screens/search/search_screen.dart';
 import '../screens/profile/profile_screen.dart';
 
@@ -12,11 +14,12 @@ class BottomNav extends StatefulWidget {
 }
 
 class _BottomNavState extends State<BottomNav> {
-  int index = 0;
+  int _index = 0;
 
-  final pages = const [
+  final List<Widget> _pages = const [
     HomeScreen(),
     ReelsScreen(),
+    ChatListScreen(),
     SearchScreen(),
     ProfileScreen(),
   ];
@@ -24,13 +27,23 @@ class _BottomNavState extends State<BottomNav> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: pages[index],
+      body: IndexedStack(
+        index: _index,
+        children: _pages,
+      ),
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: index,
+        currentIndex: _index,
         type: BottomNavigationBarType.fixed,
+        backgroundColor: const Color(0xFF0B0F1A),
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white54,
         showSelectedLabels: false,
         showUnselectedLabels: false,
-        onTap: (i) => setState(() => index = i),
+        onTap: (i) {
+          setState(() {
+            _index = i;
+          });
+        },
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home_outlined),
@@ -41,6 +54,11 @@ class _BottomNavState extends State<BottomNav> {
             icon: Icon(Icons.movie_outlined),
             activeIcon: Icon(Icons.movie),
             label: 'Reels',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.send_outlined),
+            activeIcon: Icon(Icons.send),
+            label: 'Chat',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.search),
