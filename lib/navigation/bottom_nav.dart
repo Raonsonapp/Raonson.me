d9fe3ee34a78f5f2a11d8dd199bcc21bc4268eed
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 21:22:22 +0500

    Update bottom_nav.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/navigation/bottom_nav.dart
+++ lib/navigation/bottom_nav.dart
@@ -3,6 +3,7 @@ import '../screens/home/home_screen.dart';
 import '../screens/chat/chat_list_screen.dart';
 import '../screens/reels/reels_screen.dart';
 import '../screens/search/search_screen.dart';
+import '../screens/profile/profile_screen.dart';
 
 class BottomNav extends StatefulWidget {
   const BottomNav({super.key});
@@ -14,6 +15,8 @@ class BottomNav extends StatefulWidget {
 class _BottomNavState extends State<BottomNav> {
   int _index = 0;
 
+  final PageController _pageController = PageController();
+
   final List<Widget> _screens = const [
     HomeScreen(),
     ChatListScreen(),
@@ -22,75 +25,69 @@ class _BottomNavState extends State<BottomNav> {
     ProfileScreen(),
   ];
 
+  void _onTap(int i) {
+    setState(() => _index = i);
+    _pageController.jumpToPage(i);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _screens[_index],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _index,
-        onTap: (i) => setState(() => _index = i),
-        type: BottomNavigationBarType.fixed,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        backgroundColor: const Color(0xFF0B0F1A),
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.grey,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_outlined),
-            activeIcon: Icon(Icons.home),
-            label: 'Home',
+      body: PageView(
+        controller: _pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: _screens,
+      ),
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          color: Color(0xFF0F1424),
+          border: Border(
+            top: BorderSide(color: Colors.white12, width: 0.5),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat_bubble_outline),
-            activeIcon: Icon(Icons.chat_bubble),
-            label: 'Chat',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.movie_outlined),
-            activeIcon: Icon(Icons.movie),
-            label: 'Reels',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_outline),
-            activeIcon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _index,
+          onTap: _onTap,
+          backgroundColor: const Color(0xFF0F1424),
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.white54,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              activeIcon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat_bubble_outline),
+              activeIcon: Icon(Icons.chat_bubble),
+              label: 'Chat',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.play_circle_outline),
+              activeIcon: Icon(Icons.play_circle),
+              label: 'Reels',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline),
+              activeIcon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+        ),
       ),
     );
   }
-}
-
-// --- placeholders (қадамҳои баъдӣ пур мешаванд) ---
-class _ChatPlaceholder extends StatelessWidget {
-  const _ChatPlaceholder();
-  @override
-  Widget build(BuildContext context) =>
-      const Center(child: Text('CHAT (қадамҳои баъдӣ)'));
-}
-
-class _ReelsPlaceholder extends StatelessWidget {
-  const _ReelsPlaceholder();
-  @override
-  Widget build(BuildContext context) =>
-      const Center(child: Text('REELS (қадамҳои баъдӣ)'));
-}
-
-class _SearchPlaceholder extends StatelessWidget {
-  const _SearchPlaceholder();
-  @override
-  Widget build(BuildContext context) =>
-      const Center(child: Text('SEARCH (қадамҳои баъдӣ)'));
-}
-
-class _ProfilePlaceholder extends StatelessWidget {
-  const _ProfilePlaceholder();
-  @override
-  Widget build(BuildContext context) =>
-      const Center(child: Text('PROFILE (қадамҳои баъдӣ)'));
 }
