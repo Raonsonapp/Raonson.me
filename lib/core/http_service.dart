6b06d798649163a104fef1143c81db378db5e8d9
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 17:48:07 +0500

    Update http_service.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/core/http_service.dart
+++ lib/core/http_service.dart
@@ -1,18 +1,34 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'api.dart';
+import 'session.dart';
 
 class HttpService {
-  static const base = 'https://raonson-me.onrender.com';
+  static Future<http.Response> get(String path) async {
+    final token = await Session.getToken();
 
-  static Future<http.Response> post(String path, Map body) {
-    return http.post(
-      Uri.parse(base + path),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode(body),
+    return await http.get(
+      Uri.parse(Api.baseUrl + path),
+      headers: {
+        'Content-Type': 'application/json',
+        if (token != null) 'Authorization': 'Bearer $token',
+      },
     );
   }
 
-  static Future<http.Response> get(String path) {
-    return http.get(Uri.parse(base + path));
+  static Future<http.Response> post(
+    String path, {
+    Map<String, dynamic>? body,
+  }) async {
+    final token = await Session.getToken();
+
+    return await http.post(
+      Uri.parse(Api.baseUrl + path),
+      headers: {
+        'Content-Type': 'application/json',
+        if (token != null) 'Authorization': 'Bearer $token',
+      },
+      body: jsonEncode(body ?? {}),
+    );
   }
 }
