3d185e8b85bccdba2c034b63dc20118a545b29b7
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 15:12:45 +0500

    Update bottom_nav.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/navigation/bottom_nav.dart
+++ lib/navigation/bottom_nav.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+// Screens
 import '../screens/home/home_screen.dart';
 import '../screens/reels/reels_screen.dart';
 import '../screens/search/search_screen.dart';
@@ -7,36 +9,98 @@ import '../screens/profile/profile_screen.dart';
 
 class BottomNav extends StatefulWidget {
   const BottomNav({super.key});
-  @override State<BottomNav> createState() => _BottomNavState();
+
+  @override
+  State<BottomNav> createState() => _BottomNavState();
 }
 
 class _BottomNavState extends State<BottomNav> {
-  int i = 0;
-  final screens = const [
-    HomeScreen(),
-    ChatListScreen(),
-    ReelsScreen(),
-    SearchScreen(),
-    ProfileScreen(),
+  int _currentIndex = 0;
+
+  // ================= SCREENS =================
+  final List<Widget> _screens = const [
+    HomeScreen(),        // 0
+    ChatListScreen(),    // 1
+    ReelsScreen(),       // 2
+    SearchScreen(),      // 3
+    ProfileScreen(),     // 4
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: screens[i],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: i,
-        onTap: (v) => setState(() => i = v),
-        type: BottomNavigationBarType.fixed,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
-          BottomNavigationBarItem(icon: Icon(Icons.send), label: ''),
-          BottomNavigationBarItem(icon: Icon(Icons.play_circle), label: ''),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: ''),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: ''),
+      backgroundColor: const Color(0xFF0B0F1A),
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _screens,
+      ),
+      bottomNavigationBar: _bottomBar(),
+    );
+  }
+
+  // ================= BOTTOM BAR =================
+  Widget _bottomBar() {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Color(0xFF0B0F1A),
+        boxShadow: [
+          BoxShadow(
+            color: Color(0x663AA0FF), // glow кабуд ба боло
+            blurRadius: 18,
+            offset: Offset(0, -6),
+          ),
         ],
+      ),
+      child: SafeArea(
+        top: false,
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() => _currentIndex = index);
+          },
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: const Color(0xFF0B0F1A),
+          elevation: 0,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          selectedItemColor: const Color(0xFF3AA0FF),
+          unselectedItemColor: Colors.white38,
+          items: const [
+            // HOME
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              activeIcon: Icon(Icons.home),
+              label: '',
+            ),
+
+            // CHAT (мисли Instagram airplane)
+            BottomNavigationBarItem(
+              icon: Icon(Icons.send_outlined),
+              activeIcon: Icon(Icons.send),
+              label: '',
+            ),
+
+            // REELS
+            BottomNavigationBarItem(
+              icon: Icon(Icons.play_circle_outline),
+              activeIcon: Icon(Icons.play_circle),
+              label: '',
+            ),
+
+            // SEARCH
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: '',
+            ),
+
+            // PROFILE
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline),
+              activeIcon: Icon(Icons.person),
+              label: '',
+            ),
+          ],
+        ),
       ),
     );
   }
