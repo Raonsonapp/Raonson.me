cfccbe4f9a551a643b3371b38143315434a47891
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 14:07:53 +0500

    Update main.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'core/session.dart';
+import 'screens/auth/login_screen.dart';
+import 'widgets/bottom_nav.dart';
 
-import 'navigation/bottom_nav.dart';
-import 'auth/login_screen.dart';
-
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const RaonsonApp());
+  final logged = await Session.isLoggedIn();
+  runApp(MyApp(startHome: logged));
 }
 
-class RaonsonApp extends StatelessWidget {
-  const RaonsonApp({super.key});
+class MyApp extends StatelessWidget {
+  final bool startHome;
+  const MyApp({super.key, required this.startHome});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Raonson',
       theme: ThemeData(
         brightness: Brightness.dark,
         scaffoldBackgroundColor: const Color(0xFF0B0F1A),
-        useMaterial3: true,
       ),
-      home: const Root(),
+      home: startHome ? const BottomNav() : const LoginScreen(),
     );
-  }
-}
-
-class Root extends StatefulWidget {
-  const Root({super.key});
-
-  @override
-  State<Root> createState() => _RootState();
-}
-
-class _RootState extends State<Root> {
-  bool? loggedIn;
-
-  @override
-  void initState() {
-    super.initState();
-    _checkSession();
-  }
-
-  Future<void> _checkSession() async {
-    final prefs = await SharedPreferences.getInstance();
-    final username = prefs.getString('username');
-    setState(() {
-      loggedIn = username != null;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (loggedIn == null) {
-      return const Scaffold(
-        body: Center(child: CircularProgressIndicator()),
-      );
-    }
-
-    return loggedIn!
-        ? const BottomNav()
-        : const LoginScreen();
   }
 }
