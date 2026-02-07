51c9f462f5513ec035163aa63e9b61ede70384e0
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 07:18:18 +0500

    Update bottom_nav.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/navigation/bottom_nav.dart
+++ lib/navigation/bottom_nav.dart
@@ -1,37 +1,73 @@
 import 'package:flutter/material.dart';
-bottomNavigationBar: BottomNavigationBar(
-  currentIndex: index,
-  onTap: (i) => setState(() => index = i),
-  backgroundColor: const Color(0xFF0B0F1A),
-  selectedItemColor: const Color(0xFF4DA3FF),
-  unselectedItemColor: Colors.white38,
-  type: BottomNavigationBarType.fixed,
-  showSelectedLabels: false,
-  showUnselectedLabels: false,
-  items: const [
-    BottomNavigationBarItem(
-      icon: Icon(Icons.home_outlined),
-      activeIcon: Icon(Icons.home),
-      label: '',
-    ),
-    BottomNavigationBarItem(
-      icon: Icon(Icons.play_circle_outline),
-      activeIcon: Icon(Icons.play_circle),
-      label: '',
-    ),
-    BottomNavigationBarItem(
-      icon: Icon(Icons.search),
-      label: '',
-    ),
-    BottomNavigationBarItem(
-      icon: Icon(Icons.chat_bubble_outline),
-      activeIcon: Icon(Icons.chat_bubble),
-      label: '',
-    ),
-    BottomNavigationBarItem(
-      icon: Icon(Icons.person_outline),
-      activeIcon: Icon(Icons.person),
-      label: '',
-    ),
-  ],
-),
+
+import '../screens/home/home_screen.dart';
+import '../screens/reels/reels_screen.dart';
+import '../screens/search/search_screen.dart';
+import '../screens/chat/chat_list_screen.dart';
+import '../screens/profile/profile_screen.dart';
+
+class BottomNav extends StatefulWidget {
+  const BottomNav({super.key});
+
+  @override
+  State<BottomNav> createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  int index = 0;
+
+  final List<Widget> pages = const [
+    HomeScreen(),
+    ReelsScreen(),
+    SearchScreen(),
+    ChatListScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[index],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        onTap: (i) {
+          setState(() {
+            index = i;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: const Color(0xFF4DA3FF),
+        unselectedItemColor: Colors.white38,
+        backgroundColor: const Color(0xFF0B0F1A),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            activeIcon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.play_circle_outline),
+            activeIcon: Icon(Icons.play_circle),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat_bubble_outline),
+            activeIcon: Icon(Icons.chat_bubble),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            activeIcon: Icon(Icons.person),
+            label: '',
+          ),
+        ],
+      ),
+    );
+  }
+}
