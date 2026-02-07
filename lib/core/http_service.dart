5f8e1b2cfb1e81da786d236258e6a108f1ec9f3e
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 14:20:47 +0500

    Update http_service.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/core/http_service.dart
+++ lib/core/http_service.dart
@@ -2,18 +2,25 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class HttpService {
-  static Future<http.Response> get(String url) async {
-    return await http.get(Uri.parse(url));
+  static const String baseUrl = 'https://raonson-me.onrender.com';
+
+  static Future<http.Response> get(String path) async {
+    final url = Uri.parse('$baseUrl$path');
+    return await http.get(url, headers: _headers());
   }
 
-  static Future<http.Response> post(
-    String url,
-    Map<String, dynamic> body,
-  ) async {
+  static Future<http.Response> post(String path, Map<String, dynamic> body) async {
+    final url = Uri.parse('$baseUrl$path');
     return await http.post(
-      Uri.parse(url),
-      headers: {'Content-Type': 'application/json'},
+      url,
+      headers: _headers(),
       body: jsonEncode(body),
     );
+  }
+
+  static Map<String, String> _headers() {
+    return {
+      'Content-Type': 'application/json',
+    };
   }
 }
