478cdc9b80aae76c8b573de6ab378e165c7a0cc5
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 11:18:37 +0500

    Update api.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/core/api.dart
+++ lib/core/api.dart
@@ -1,21 +1,3 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-import 'auth_state.dart';
-
-const baseUrl = "https://raonson-me.onrender.com";
-
-Future<bool> login(String u, String p) async {
-  final res = await http.post(
-    Uri.parse("$baseUrl/auth/login"),
-    headers: {"Content-Type": "application/json"},
-    body: jsonEncode({"username": u, "password": p}),
-  );
-
-  if (res.statusCode == 200) {
-    final data = jsonDecode(res.body);
-    AuthState.token = data["token"];
-    AuthState.username = data["user"]["username"];
-    return true;
-  }
-  return false;
+class ApiConfig {
+  static const String baseUrl = 'https://raonson-me.onrender.com';
 }
