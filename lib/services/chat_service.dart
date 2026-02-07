7a379d20d0dba3ff5224e6e0d50032ffdd34ebec
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 21:04:30 +0500

    Update chat_service.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/services/chat_service.dart
+++ lib/services/chat_service.dart
@@ -1,29 +1,33 @@
-import 'dart:convert';
 import '../core/api.dart';
 import '../core/http_service.dart';
-import '../models/message.dart';
 
 class ChatService {
-  static Future<List<String>> chats() async {
-    final res = await HttpService.get(Api.chats);
-    final List data = jsonDecode(res.body);
-    return data.map((e) => e.toString()).toList();
+  // ===== GET CHATS =====
+  static Future<List<dynamic>> getChats(String username) async {
+    final res = await HttpService.get(
+      '${Api.baseUrl}/chats/$username',
+    );
+    return (res as List?) ?? [];
   }
 
-  static Future<List<Message>> messages(String user) async {
-    final res = await HttpService.get('${Api.messages}/$user');
-    final List data = jsonDecode(res.body);
-    return data.map((e) => Message.fromJson(e)).toList();
+  // ===== GET MESSAGES =====
+  static Future<List<dynamic>> getMessages(String chatId) async {
+    final res = await HttpService.get(
+      '${Api.baseUrl}/chats/$chatId/messages',
+    );
+    return (res as List?) ?? [];
   }
 
-  static Future<void> send({
-    required String to,
+  // ===== SEND MESSAGE =====
+  static Future<void> sendMessage({
+    required String chatId,
+    required String from,
     required String text,
   }) async {
     await HttpService.post(
-      Api.sendMessage,
-      body: {
-        'to': to,
+      '${Api.baseUrl}/chats/$chatId/messages',
+      {
+        'from': from,
         'text': text,
       },
     );
