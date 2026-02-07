0713b1a7e42760e087acb1affd3ef0c04854df19
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 15:19:22 +0500

    Update session.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/core/session.dart
+++ lib/core/session.dart
@@ -1,18 +1,29 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Session {
-  static Future<void> save(String user) async {
-    final p = await SharedPreferences.getInstance();
-    await p.setString('user', user);
+  static const String _keyUser = 'raonson_user';
+
+  // SAVE USER
+  static Future<void> save(String username) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_keyUser, username);
   }
 
+  // GET USER
   static Future<String?> username() async {
-    final p = await SharedPreferences.getInstance();
-    return p.getString('user');
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_keyUser);
   }
 
+  // CHECK LOGIN
+  static Future<bool> isLoggedIn() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.containsKey(_keyUser);
+  }
+
+  // LOGOUT
   static Future<void> logout() async {
-    final p = await SharedPreferences.getInstance();
-    await p.clear();
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(_keyUser);
   }
 }
