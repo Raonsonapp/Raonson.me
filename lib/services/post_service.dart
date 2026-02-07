f6470fe73b997f1c545dfd11b6500c72e4300a91
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 14:26:58 +0500

    Update post_service.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/services/post_service.dart
+++ lib/services/post_service.dart
@@ -3,18 +3,17 @@ import '../core/http_service.dart';
 import '../models/post_model.dart';
 
 class PostService {
-  static Future<List<PostModel>> fetchPosts() async {
+  static Future<List<PostModel>> getPosts() async {
     final res = await HttpService.get('/posts');
 
     if (res.statusCode == 200) {
       final List data = jsonDecode(res.body);
       return data.map((e) => PostModel.fromJson(e)).toList();
-    } else {
-      throw Exception('Failed to load posts');
     }
+    throw Exception('Failed to load posts');
   }
 
-  static Future<void> likePost(int postId) async {
+  static Future<void> like(int postId) async {
     await HttpService.post('/posts/$postId/like', {});
   }
 }
