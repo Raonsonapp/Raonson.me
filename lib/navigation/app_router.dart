b89dd11a6dc776413a4a40443e2c11b7b4dcb89a
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 07:18:37 +0500

    Update app_router.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/navigation/app_router.dart
+++ lib/navigation/app_router.dart
@@ -2,28 +2,11 @@ import 'package:flutter/material.dart';
 import '../screens/auth/login_screen.dart';
 import 'bottom_nav.dart';
 
-class AppRouter extends StatefulWidget {
+class AppRouter extends StatelessWidget {
   const AppRouter({super.key});
 
   @override
-  State<AppRouter> createState() => _AppRouterState();
-}
-
-class _AppRouterState extends State<AppRouter> {
-  bool isLoggedIn = false; // v2: баъд аз server
-
-  @override
   Widget build(BuildContext context) {
-    if (isLoggedIn) {
-      return const BottomNav();
-    } else {
-      return LoginScreen(
-        onLoginSuccess: () {
-          setState(() {
-            isLoggedIn = true;
-          });
-        },
-      );
-    }
+    return const LoginScreen();
   }
 }
