dac3a3a5aa1597c104d96edc000430b64ab1f363
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 14:58:36 +0500

    Update main.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,50 @@
 import 'package:flutter/material.dart';
 import 'core/session.dart';
+import 'navigation/bottom_nav.dart';
 import 'screens/auth/login_screen.dart';
-import 'screens/home/home_screen.dart';
 
 void main() {
-  runApp(const RaonsonApp());
+  runApp(const App());
 }
 
-class RaonsonApp extends StatelessWidget {
-  const RaonsonApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: const _Bootstrap(),
+      home: const Bootstrap(),
     );
   }
 }
 
-class _Bootstrap extends StatefulWidget {
-  const _Bootstrap();
+class Bootstrap extends StatefulWidget {
+  const Bootstrap({super.key});
 
   @override
-  State<_Bootstrap> createState() => _BootstrapState();
+  State<Bootstrap> createState() => _BootstrapState();
 }
 
-class _BootstrapState extends State<_Bootstrap> {
-  bool loading = true;
-  bool logged = false;
-
+class _BootstrapState extends State<Bootstrap> {
   @override
   void initState() {
     super.initState();
-    _check();
+    _go();
   }
 
-  Future<void> _check() async {
-    logged = await Session.isLoggedIn();
-    setState(() => loading = false);
+  Future<void> _go() async {
+    final u = await Session.username();
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (_) => u == null ? const LoginScreen() : const BottomNav(),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    if (loading) {
-      return const Scaffold(
-        body: Center(child: CircularProgressIndicator()),
-      );
-    }
-    return logged ? const HomeScreen() : const LoginScreen();
+    return const Scaffold(body: Center(child: CircularProgressIndicator()));
   }
 }
