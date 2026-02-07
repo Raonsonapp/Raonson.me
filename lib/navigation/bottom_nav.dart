49c396f27ff65f64d787f34eb0466508b4bc9bfe
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 20:45:24 +0500

    Update bottom_nav.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/navigation/bottom_nav.dart
+++ lib/navigation/bottom_nav.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-
 import '../screens/home/home_screen.dart';
-import '../screens/reels/reels_screen.dart';
-import '../screens/chat/chat_list_screen.dart';
-import '../screens/search/search_screen.dart';
-import '../screens/profile/profile_screen.dart';
 
 class BottomNav extends StatefulWidget {
   const BottomNav({super.key});
@@ -16,34 +11,27 @@ class BottomNav extends StatefulWidget {
 class _BottomNavState extends State<BottomNav> {
   int _index = 0;
 
-  final List<Widget> _pages = const [
+  final List<Widget> _screens = const [
     HomeScreen(),
-    ReelsScreen(),
-    ChatListScreen(),
-    SearchScreen(),
-    ProfileScreen(),
+    _ChatPlaceholder(),
+    _ReelsPlaceholder(),
+    _SearchPlaceholder(),
+    _ProfilePlaceholder(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IndexedStack(
-        index: _index,
-        children: _pages,
-      ),
+      body: _screens[_index],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _index,
+        onTap: (i) => setState(() => _index = i),
         type: BottomNavigationBarType.fixed,
-        backgroundColor: const Color(0xFF0B0F1A),
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.white54,
         showSelectedLabels: false,
         showUnselectedLabels: false,
-        onTap: (i) {
-          setState(() {
-            _index = i;
-          });
-        },
+        backgroundColor: const Color(0xFF0B0F1A),
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home_outlined),
@@ -51,14 +39,14 @@ class _BottomNavState extends State<BottomNav> {
             label: 'Home',
           ),
           BottomNavigationBarItem(
+            icon: Icon(Icons.chat_bubble_outline),
+            activeIcon: Icon(Icons.chat_bubble),
+            label: 'Chat',
+          ),
+          BottomNavigationBarItem(
             icon: Icon(Icons.movie_outlined),
             activeIcon: Icon(Icons.movie),
             label: 'Reels',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.send_outlined),
-            activeIcon: Icon(Icons.send),
-            label: 'Chat',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.search),
@@ -73,4 +61,33 @@ class _BottomNavState extends State<BottomNav> {
       ),
     );
   }
+}
+
+// --- placeholders (қадамҳои баъдӣ пур мешаванд) ---
+class _ChatPlaceholder extends StatelessWidget {
+  const _ChatPlaceholder();
+  @override
+  Widget build(BuildContext context) =>
+      const Center(child: Text('CHAT (қадамҳои баъдӣ)'));
+}
+
+class _ReelsPlaceholder extends StatelessWidget {
+  const _ReelsPlaceholder();
+  @override
+  Widget build(BuildContext context) =>
+      const Center(child: Text('REELS (қадамҳои баъдӣ)'));
+}
+
+class _SearchPlaceholder extends StatelessWidget {
+  const _SearchPlaceholder();
+  @override
+  Widget build(BuildContext context) =>
+      const Center(child: Text('SEARCH (қадамҳои баъдӣ)'));
+}
+
+class _ProfilePlaceholder extends StatelessWidget {
+  const _ProfilePlaceholder();
+  @override
+  Widget build(BuildContext context) =>
+      const Center(child: Text('PROFILE (қадамҳои баъдӣ)'));
 }
