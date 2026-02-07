6815948ef20a64866574bf6434483a0148cc99ba
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 13:59:18 +0500

    Update bottom_nav.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/navigation/bottom_nav.dart
+++ lib/navigation/bottom_nav.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 
-import '../screens/home/home_screen.dart';
-import '../screens/reels/reels_screen.dart';
-import '../screens/search/search_screen.dart';
-import '../screens/profile/profile_screen.dart';
-import '../screens/chat/chat_list_screen.dart';
-import '../screens/add/add_post_screen.dart';
+import '../home/home_screen.dart';
+import '../reels/reels_screen.dart';
+import '../search/search_screen.dart';
+import '../profile/profile_screen.dart';
 
 class BottomNav extends StatefulWidget {
   const BottomNav({super.key});
@@ -17,114 +15,53 @@ class BottomNav extends StatefulWidget {
 class _BottomNavState extends State<BottomNav> {
   int index = 0;
 
-  final List<Widget> screens = const [
-    HomeScreen(),        // 0
-    ReelsScreen(),       // 1
-    SizedBox(),          // 2 -> Add handled separately
-    SearchScreen(),      // 3
-    ProfileScreen(),     // 4
+  final pages = const [
+    HomeScreen(),
+    ReelsScreen(),
+    SizedBox(), // Share handled from Home (+)
+    SearchScreen(),
+    ProfileScreen(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF0B0F1A),
-      body: screens[index],
-      bottomNavigationBar: Container(
-        height: 72,
-        decoration: BoxDecoration(
-          color: const Color(0xFF0B0F1A),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.blueAccent.withOpacity(0.35),
-              blurRadius: 28,
-              offset: const Offset(0, -6),
-            ),
-          ],
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            _navItem(
-              icon: Icons.home_outlined,
-              active: Icons.home,
-              i: 0,
-            ),
-            _navItem(
-              icon: Icons.play_circle_outline,
-              active: Icons.play_circle,
-              i: 1,
-            ),
-            _addButton(context),
-            _navItem(
-              icon: Icons.search,
-              active: Icons.search,
-              i: 3,
-            ),
-            _navItem(
-              icon: Icons.person_outline,
-              active: Icons.person,
-              i: 4,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _navItem({
-    required IconData icon,
-    required IconData active,
-    required int i,
-  }) {
-    final bool selected = index == i;
-
-    return GestureDetector(
-      onTap: () {
-        setState(() => index = i);
-      },
-      child: Icon(
-        selected ? active : icon,
-        size: 26,
-        color: selected ? Colors.blueAccent : Colors.white54,
-      ),
-    );
-  }
-
-  Widget _addButton(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (_) => const AddPostScreen(),
+      body: pages[index],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        onTap: (i) {
+          if (i == 2) return; // + НЕ дар bottom
+          setState(() => index = i);
+        },
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        backgroundColor: const Color(0xFF0B0F1A),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            activeIcon: Icon(Icons.home),
+            label: 'Home',
           ),
-        );
-      },
-      child: Container(
-        width: 56,
-        height: 56,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          gradient: const LinearGradient(
-            colors: [
-              Colors.blueAccent,
-              Colors.lightBlue,
-            ],
+          BottomNavigationBarItem(
+            icon: Icon(Icons.play_circle_outline),
+            activeIcon: Icon(Icons.play_circle),
+            label: 'Reels',
           ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.blueAccent.withOpacity(0.7),
-              blurRadius: 22,
-              spreadRadius: 2,
-            ),
-          ],
-        ),
-        child: const Icon(
-          Icons.add,
-          size: 32,
-          color: Colors.white,
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle_outline),
+            label: 'Add',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            activeIcon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
       ),
     );
   }
