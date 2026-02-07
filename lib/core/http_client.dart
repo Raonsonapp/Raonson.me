95dfdc60ddb17795c50e31df668634a4e6d493a7
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 12:37:01 +0500

    Update http_client.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/core/http_client.dart
+++ lib/core/http_client.dart
@@ -1,24 +1,16 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-class HttpClient {
-  static Future<dynamic> get(String url) async {
-    final res = await http.get(Uri.parse(url));
-    if (res.statusCode == 200) {
-      return jsonDecode(res.body);
-    }
-    throw Exception('GET failed');
+class HttpService {
+  static Future<http.Response> get(String url) async {
+    return await http.get(Uri.parse(url));
   }
 
-  static Future<dynamic> post(String url, Map body) async {
-    final res = await http.post(
+  static Future<http.Response> post(String url, Map<String, dynamic> body) async {
+    return await http.post(
       Uri.parse(url),
       headers: {'Content-Type': 'application/json'},
       body: jsonEncode(body),
     );
-    if (res.statusCode == 200) {
-      return jsonDecode(res.body);
-    }
-    throw Exception('POST failed');
   }
 }
