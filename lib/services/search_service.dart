1558cb08a3a8344cb097adf741b1560557cd09db
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 06:43:38 +0500

    Update search_service.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/services/search_service.dart
+++ lib/services/search_service.dart
@@ -1,12 +1,16 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../config/api.dart';
+import '../core/config.dart';
 
 class SearchService {
-  static Future<Map<String, dynamic>> search(String q) async {
-    final res = await http.get(
-      Uri.parse("$baseUrl/search?q=$q"),
-    );
-    return jsonDecode(res.body);
+  static Future<List> searchPosts() async {
+    final res = await http
+        .get(Uri.parse('${AppConfig.baseUrl}/search'))
+        .timeout(const Duration(seconds: 10));
+
+    if (res.statusCode == 200) {
+      return jsonDecode(res.body);
+    }
+    throw Exception('Search failed');
   }
 }
