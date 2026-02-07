250e561d02b52b9578e5103485527e9f08477d86
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-07 15:02:47 +0500

    Update home_screen.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,132 +1,61 @@
 import 'package:flutter/material.dart';
-import '../../models/post_model.dart';
 import '../../services/post_service.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  @override State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<PostModel> posts = [];
-  bool loading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _load();
-  }
-
-  Future<void> _load() async {
-    final data = await PostService.getPosts();
-    setState(() {
-      posts = data;
-      loading = false;
-    });
+  List posts = [];
+  @override void initState(){ super.initState(); load(); }
+  Future<void> load() async {
+    posts = await PostService.getPosts();
+    setState((){});
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext c) {
     return Scaffold(
-      backgroundColor: const Color(0xFF0B0F1A),
       appBar: AppBar(
-        backgroundColor: const Color(0xFF0B0F1A),
-        elevation: 0,
-        leading: IconButton(
-          icon: const Icon(Icons.add),
-          onPressed: () {},
-        ),
-        title: const Text('Raonson'),
-        centerTitle: true,
-        actions: const [
-          Padding(
-            padding: EdgeInsets.only(right: 12),
-            child: Icon(Icons.smart_toy),
-          )
-        ],
+        title: Row(children: const [
+          Icon(Icons.add_box_outlined), SizedBox(width:8),
+          Text('Raonson')
+        ]),
+        actions: const [Icon(Icons.smart_toy)],
       ),
-      body: loading
-          ? const Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: posts.length,
-              itemBuilder: (c, i) => _PostCard(post: posts[i]),
+      body: ListView.builder(
+        itemCount: posts.length,
+        itemBuilder: (_, i) {
+          final p = posts[i];
+          return Card(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ListTile(title: Text(p['username'])),
+                Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Text(p['caption']),
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.favorite_border),
+                      onPressed: () async {
+                        await PostService.like(p['id']);
+                      },
+                    ),
+                    const Icon(Icons.comment),
+                    const Icon(Icons.send),
+                    const Spacer(),
+                    const Icon(Icons.bookmark_border),
+                  ],
+                )
+              ],
             ),
-    );
-  }
-}
-
-class _PostCard extends StatefulWidget {
-  final PostModel post;
-  const _PostCard({required this.post});
-
-  @override
-  State<_PostCard> createState() => _PostCardState();
-}
-
-class _PostCardState extends State<_PostCard> {
-  bool liked = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        ListTile(
-          leading: const CircleAvatar(child: Icon(Icons.person)),
-          title: Text(widget.post.username),
-          trailing: const Icon(Icons.more_vert),
-        ),
-        Container(
-          height: 320,
-          margin: const EdgeInsets.symmetric(horizontal: 12),
-          decoration: BoxDecoration(
-            color: const Color(0xFF1C2333),
-            borderRadius: BorderRadius.circular(16),
-          ),
-          child: const Center(
-            child: Icon(Icons.image, size: 80, color: Colors.white24),
-          ),
-        ),
-        Row(
-          children: [
-            IconButton(
-              icon: Icon(
-                liked ? Icons.favorite : Icons.favorite_border,
-                color: liked ? Colors.red : Colors.white,
-              ),
-              onPressed: () async {
-                await PostService.like(widget.post.id);
-                setState(() {
-                  liked = !liked;
-                  widget.post.likes++;
-                });
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.chat_bubble_outline),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: const Icon(Icons.send),
-              onPressed: () {},
-            ),
-            const Spacer(),
-            IconButton(
-              icon: const Icon(Icons.bookmark_border),
-              onPressed: () {},
-            ),
-          ],
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Text(
-            '${widget.post.username}  ${widget.post.caption}',
-          ),
-        ),
-        const SizedBox(height: 20),
-      ],
+          );
+        },
+      ),
     );
   }
 }
